50a640ae915f796cef9cb6a7a82595e6a30c7111
Brad Wood <brad@bradwood.com>
2023-07-16 06:52:59 +0200

    Flesh out type hierarcgy

--- runtime/src/main/java/ortus/boxlang/runtime/types/IType.java
+++ runtime/src/main/java/ortus/boxlang/runtime/types/IType.java
@@ -2,7 +2,32 @@ package ortus.boxlang.runtime.types;
 
 /**
  * Represents a base type
+ *
+ * Type Hierarchy
+ * - Struct
+ *  - sorted, ordered, etc
+ * - Array
+ * - XML
+ * - Query
+ * - Simple
+ *   - String
+ *   - Numeric
+ *   - Boolean
+ *   - List
+ *   - Date
  */
 public interface IType {
 
+    /**
+     * Represent as string, or throw exception if not possible
+     */
+    public String asString();
+
+    // These come from the Object class, but will be important
+
+    // toString()
+
+    // hashcode()
+
+    // equals()
 }
