51229ff564a2d0aebc3466dcee18aa7775170286
fabio <fabio.moriero@strumenta.com>
2023-07-31 18:46:09 +0200

    formatting

--- compiler/src/main/kotlin/ortus/boxlang/parser/cfscriptToAst.kt
+++ compiler/src/main/kotlin/ortus/boxlang/parser/cfscriptToAst.kt
@@ -1,27 +1,25 @@
 package ortus.boxlang.parser
 
-import ortus.boxlang.parser.CFScript
-
 // TODO: partially implemented
-fun CFParser.ScriptContext.toAst() : CFScript {
-    val statements = mutableListOf<Statement>()
+fun CFParser.ScriptContext.toAst(): CFScript {
+	val statements = mutableListOf<Statement>()
 
-    if (this.component() != null)
-        statements += this.component().toAst()
+	if (this.component() != null)
+		statements += this.component().toAst()
 
-    return CFScript(statements)
+	return CFScript(statements)
 }
 
 // TODO: partially implemented
 fun CFParser.ComponentContext.toAst() = Component(
-    identifier = this.identifier()?.text ?: "",
-    functions = this.functionOrStatement()
-        .filter { it.function() != null }
-        .map { it.function().toAst() }
+	identifier = this.identifier()?.text ?: "",
+	functions = this.functionOrStatement()
+		.filter { it.function() != null }
+		.map { it.function().toAst() }
 )
 
 // TODO: partially implemented
 fun CFParser.FunctionContext.toAst() = Function(
-    identifier = this.functionSignature().identifier().text,
-    returnType = this.functionSignature().returnType()?.text ?: ""
+	identifier = this.functionSignature().identifier().text,
+	returnType = this.functionSignature().returnType()?.text ?: ""
 )
