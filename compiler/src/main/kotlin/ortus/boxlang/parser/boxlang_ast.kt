27ec23b6ca34f5ad4199edb55b4d338e1bd6cd80
fabio <fabio.moriero@strumenta.com>
2023-08-01 18:33:44 +0200

    [compiler] WIP - AST modelling

--- compiler/src/main/kotlin/ortus/boxlang/parser/boxlang_ast.kt
+++ compiler/src/main/kotlin/ortus/boxlang/parser/boxlang_ast.kt
@@ -1,23 +1,71 @@
 package ortus.boxlang.parser
 
+import com.strumenta.kolasu.model.Named
 import com.strumenta.kolasu.model.Node
+import com.strumenta.kolasu.model.ReferenceByName
+
+sealed class BoxNode : Node()
 
 data class CFScript(
-    val body : List<Statement>
+	val body: List<Statement>
 ) : Node()
 
-open class Statement(
-    val dummy : String = ""
-) : Node()
+sealed class Statement : BoxNode()
+
+data class Assignment(
+	val left: Expression,
+	val right: Expression
+) : Statement()
 
 data class Function(
-    val identifier: String,
-    val returnType: String,
-    val parameters: List<String> = emptyList(),
-    val body: List<Statement> = emptyList()
-): Node()
+	val identifier: String,
+	val returnType: String,
+	val parameters: List<String> = emptyList(),
+	val body: List<Statement> = emptyList()
+) : BoxNode()
 
 data class Component(
-    val identifier: String,
-    val functions: List<Function>
+	val identifier: String,
+	val functions: List<Function>
 ) : Statement()
+
+interface ExpressionType
+sealed class Expression(var type: ExpressionType? = null) : BoxNode()
+
+data class IntegerLiteral(val value: String) : Expression()
+data class FloatLiteral(val value: String) : Expression()
+data class StringLiteral(val value: String) : Expression()
+data class BooleanLiteral(val value: String) : Expression()
+
+data class Identifier(override val name: String) : Expression(), Named
+
+enum class BinaryOperator {
+	Concat
+}
+
+enum class ComparisonOperator {
+	Equals
+}
+
+data class BinaryExpression(
+	var left: Expression,
+	var op: BinaryOperator,
+	var right: Expression
+) : Expression()
+
+data class ComparisonExpression(
+	var left: Expression,
+	var op: ComparisonOperator,
+	var right: Expression
+) : Expression()
+
+data class ScopeVariablesExpression(
+	val key: String
+) : Expression()
+
+data class FunctionInvokationExpression(
+	val name: ReferenceByName<Identifier>,
+	val arguments: List<Expression>
+) : Expression()
+
+
