074ddb2940aed3588acaa8d2ef090e6fea6e3905
Luis Majano <lmajano@gmail.com>
2023-08-05 22:18:15 +0200

    constructors can be called now!! WOOT WOOT!

--- runtime/src/main/java/ortus/boxlang/runtime/interop/ClassInvoker.java
+++ runtime/src/main/java/ortus/boxlang/runtime/interop/ClassInvoker.java
@@ -5,20 +5,46 @@ import java.lang.invoke.ConstantCallSite;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
+/**
+ * This class is used to represent a BX/Java Class and invoke methods on classes using invoke dynamic:
+ *
+ * - invokestatic is used to invoke static methods.
+ * - invokevirtual is used to invoke public and protected non-static methods via dynamic dispatch.
+ * - invokeinterface is similar to invokevirtual except for the method dispatch being based on an interface type.
+ * - invokespecial is used to invoke instance initialization methods (constructors) as well as private methods and methods of a superclass of the
+ * current class.
+ */
 public class ClassInvoker {
+
+	// @formatter:off
+	private static final Map<Class<?>, Class<?>> PRIMITIVE_MAP = Map.of(
+            Boolean.class, boolean.class,
+            Byte.class, byte.class,
+            Character.class, char.class,
+            Short.class, short.class,
+            Integer.class, int.class,
+            Long.class, long.class,
+            Float.class, float.class,
+            Double.class, double.class
+    );
+	// @formatter:on
 
 	/**
 	 * This invokers works of this targeted class
 	 */
-	private Class<?>					targetClass;
+	private Class<?>								targetClass;
 
 	/**
 	 * This is the instance of the targeted class if it exists, since it could be static method calls or interface calls
 	 */
-	private Object						targetInstance	= null;
+	private Object									targetInstance	= null;
 
-	private final MethodHandles.Lookup	lookup			= MethodHandles.lookup();
+	private final MethodHandles.Lookup				lookup			= MethodHandles.lookup();
 
 	public ClassInvoker( Class<?> targetClass ) {
 		this.targetClass = targetClass;
@@ -64,20 +90,28 @@ public class ClassInvoker {
 	}
 
 	public Object invokeConstructor( Object... args ) throws Throwable {
-
-		system.outpu.println( args.toString() );
-
+		// Convert the arguments to an array of classes
+		Class<?>[]		argTypes			= Arrays.stream( args ).map( ClassInvoker::toClass ).toArray( Class<?>[]::new );
 		// Method signature for a constructor is void (Object...)
-		MethodType		constructorType		= MethodType.methodType( void.class, args.getClass() );
+		MethodType		constructorType		= MethodType.methodType( void.class, argTypes );
 		// Define the bootstrap method
 		MethodHandle	constructorHandle	= lookup.findConstructor( this.targetClass, constructorType );
 		// Create a callsite using the constructor handle
 		CallSite		callSite			= new ConstantCallSite( constructorHandle );
 		// Bind the CallSite and invoke the constructor with the provided arguments
 		MethodHandle	constructorInvoker	= callSite.dynamicInvoker();
-		this.targetInstance = constructorInvoker.invokeWithArguments( ( Object ) args );
+		this.targetInstance = constructorInvoker.invokeWithArguments( args );
 
 		return this.targetInstance;
+	}
+
+	public static Class<?> toClass( Object thisArg ) {
+		// TODO: Not sure what happens when the arg is null?
+		if ( thisArg == null ) {
+			return Object.class;
+		}
+		Class<?> clazz = thisArg.getClass();
+		return PRIMITIVE_MAP.getOrDefault( clazz, clazz );
 	}
 
 	public Object invoke( String methodName, Object... args ) throws Throwable {
--- runtime/src/test/java/ortus/boxlang/runtime/interop/ClassInvokerTest.java
+++ runtime/src/test/java/ortus/boxlang/runtime/interop/ClassInvokerTest.java
@@ -1,11 +1,11 @@
 package ortus.boxlang.runtime.interop;
 
-import java.lang.String;
-import org.junit.jupiter.api.Test;
-
 import ortus.boxlang.runtime.types.IType;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.lang.String;
+import java.util.LinkedHashMap;
+
+import org.junit.jupiter.api.Test;
 import static com.google.common.truth.Truth.assertThat;
 
 public class ClassInvokerTest {
@@ -36,6 +36,24 @@ public class ClassInvokerTest {
 		ClassInvoker	target	= new ClassInvoker( String.class );
 		String			results	= ( String ) target.invokeConstructor( "Hello World" );
 		assertThat( results ).isEqualTo( "Hello World" );
+		assertThat( results.getClass() ).isEqualTo( String.class );
+	}
+
+	@Test
+	@SuppressWarnings( "unchecked" )
+	void testItCanCallConstructorsWithManyArguments() throws Throwable {
+		ClassInvoker target = new ClassInvoker( LinkedHashMap.class );
+		System.out.println( int.class );
+		LinkedHashMap results = ( LinkedHashMap ) target.invokeConstructor( 16, 0.75f, true );
+		assertThat( results.getClass() ).isEqualTo( LinkedHashMap.class );
+	}
+
+	@Test
+	void testItCanCallConstructorsWithNoArguments() throws Throwable {
+		ClassInvoker	target	= new ClassInvoker( String.class );
+		String			results	= ( String ) target.invokeConstructor();
+		assertThat( results ).isEqualTo( "" );
+		assertThat( results.getClass() ).isEqualTo( String.class );
 	}
 
 }
