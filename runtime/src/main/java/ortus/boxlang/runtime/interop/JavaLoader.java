50dd7a591552282c9d2d6d7d0ad12c8335871c89
Luis Majano <lmajano@gmail.com>
2023-08-09 15:35:00 +0200

    docs

--- runtime/src/main/java/ortus/boxlang/runtime/interop/JavaLoader.java
+++ runtime/src/main/java/ortus/boxlang/runtime/interop/JavaLoader.java
@@ -1,9 +1,33 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ortus.boxlang.runtime.interop;
 
 import java.util.Optional;
 
-import ortus.boxlang.runtime.BoxRuntime;
-
+/**
+ * This class is responsible for loading Java classes into the runtime.
+ * It will try to locate the class in the runtime modules first, then
+ * in the system class loader.
+ *
+ * We can create further class loading hierarchies later, like inline loading, application.cfc etc.
+ *
+ * For now, we will just load from the system class loader.
+ */
 public class JavaLoader {
 
 	/**
@@ -56,9 +80,9 @@ public class JavaLoader {
 
 	public static ClassInvoker load( String fullyQualifiedClassName ) throws ClassNotFoundException {
 		return loadFromModules( fullyQualifiedClassName ).or( () -> loadFromSystem( fullyQualifiedClassName ) )
-				.orElseThrow( () -> new ClassNotFoundException(
-						String.format( "The requested class [%s] has not been located", fullyQualifiedClassName ) )
-				);
+		        .orElseThrow( () -> new ClassNotFoundException(
+		                String.format( "The requested class [%s] has not been located", fullyQualifiedClassName ) )
+		        );
 
 	}
 
