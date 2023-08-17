663f95089ff65e73d66282792c34fac92155f797
Luis Majano <lmajano@gmail.com>
2023-08-17 09:46:39 +0200

    Added an Assert Operator

---
+++ runtime/src/test/java/ortus/boxlang/runtime/operators/AssertTest.java
@@ -0,0 +1,46 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ortus.boxlang.runtime.operators;
+
+import org.junit.Ignore;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class AssertTest {
+
+	@DisplayName( "It can assert throw exceptions on null values" )
+	@Test
+	void testItCanThrowOnNullValues() {
+		assertThrows( AssertionError.class, () -> Assert.invoke( null ) );
+	}
+
+	@DisplayName( "It can assert throw exceptions on false values" )
+	@Test
+	void testItCanThrowOnFalseValues() {
+		assertThrows( AssertionError.class, () -> Assert.invoke( false ) );
+	}
+
+	@DisplayName( "It can assert" )
+	@Test
+	void testItCanAssert() {
+		assertThat( Assert.invoke( true ) ).isTrue();
+	}
+
+}
