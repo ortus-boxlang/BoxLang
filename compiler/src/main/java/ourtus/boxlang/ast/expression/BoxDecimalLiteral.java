c83c5638ea60120dd6a092dce6ee8e316d6ceacf
mctaverna <maurizio.taverna@strumenta.com>
2023-09-11 11:55:37 +0200

    [compiler] Added Decimal node

---
+++ compiler/src/main/java/ourtus/boxlang/ast/expression/BoxDecimalLiteral.java
@@ -0,0 +1,39 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package ourtus.boxlang.ast.expression;
+
+import ourtus.boxlang.ast.BoxExpr;
+import ourtus.boxlang.ast.Node;
+import ourtus.boxlang.ast.Position;
+
+public class BoxDecimalLiteral extends BoxExpr {
+
+	private final String value;
+
+	public String getValue() {
+		return value;
+	}
+	/**
+	 * Terminal node DecimalLiteral
+	 * @param value
+	 * @param position
+	 * @param sourceText
+	 */
+	public BoxDecimalLiteral(String value, Position position, String sourceText ) {
+		super( position, sourceText );
+		this.value =  value;
+	}
+
+}
