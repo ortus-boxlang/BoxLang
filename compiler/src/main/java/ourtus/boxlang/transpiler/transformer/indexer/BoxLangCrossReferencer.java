fd45885c2ab651ca3e4d406eafec656955148d27
mctaverna <maurizio.taverna@strumenta.com>
2023-09-11 12:14:54 +0200

    [compiler] Added cross-reference

---
+++ compiler/src/main/java/ourtus/boxlang/transpiler/transformer/indexer/BoxLangCrossReferencer.java
@@ -0,0 +1,38 @@
+package ourtus.boxlang.transpiler.transformer.indexer;
+
+import com.github.javaparser.ast.Node;
+
+import java.io.File;
+
+public abstract class BoxLangCrossReferencer {
+	protected File source;
+	protected File destination;
+	protected boolean enabled;
+	public abstract Node storeReference(Node javaNode, ourtus.boxlang.ast.BoxNode boxNode);
+
+	public BoxLangCrossReferencer() {
+		this.enabled = true;
+	}
+
+	public BoxLangCrossReferencer(File source, File destination) {
+		this.source = source;
+		this.destination = destination;
+		this.enabled = true;
+	}
+
+	public File getSource() {
+		return source;
+	}
+
+	public void setSource(File source) {
+		this.source = source;
+	}
+
+	public File getDestination() {
+		return destination;
+	}
+
+	public void setDestination(File destination) {
+		this.destination = destination;
+	}
+}
---
+++ compiler/src/main/java/ourtus/boxlang/transpiler/transformer/indexer/BoxLangCrossReferencerDefault.java
@@ -0,0 +1,20 @@
+package ourtus.boxlang.transpiler.transformer.indexer;
+
+import com.github.javaparser.Position;
+import com.github.javaparser.Range;
+import com.github.javaparser.ast.Node;
+
+public class BoxLangCrossReferencerDefault extends BoxLangCrossReferencer {
+
+	public Node storeReference(Node javaNode, ourtus.boxlang.ast.BoxNode boxNode) {
+		if(this.enabled) {
+			Range range = new Range(
+				new Position(boxNode.getPosition().getStart().getLine(),boxNode.getPosition().getStart().getColumn()),
+				new Position(boxNode.getPosition().getEnd().getLine(),boxNode.getPosition().getEnd().getColumn())
+			);
+			javaNode.setRange(range);
+		}
+		return javaNode;
+	}
+
+}
