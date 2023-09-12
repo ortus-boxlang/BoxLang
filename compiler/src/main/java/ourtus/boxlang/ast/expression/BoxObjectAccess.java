5a5a1a90ee7498464e25ab665aad10dd9828b780
mctaverna <maurizio.taverna@strumenta.com>
2023-09-12 11:31:35 +0200

    [compiler] Added documentation

--- compiler/src/main/java/ourtus/boxlang/ast/expression/BoxObjectAccess.java
+++ compiler/src/main/java/ourtus/boxlang/ast/expression/BoxObjectAccess.java
@@ -17,6 +17,11 @@ package ourtus.boxlang.ast.expression;
 import ourtus.boxlang.ast.BoxExpr;
 import ourtus.boxlang.ast.Position;
 
+
+/**
+ * AST Node representing access with a dot like:
+ * <code>object.property</code> or <code>object.method()</code>
+ */
 public class BoxObjectAccess extends BoxAccess {
 
 	private BoxExpr context;
@@ -38,6 +43,13 @@ public class BoxObjectAccess extends BoxAccess {
 		this.access = access;
 	}
 
+	/**
+	 * Creates the AST node
+	 * @param context expression representing the object
+	 * @param access expression after the dot
+	 * @param position position of the statement in the source code
+	 * @param sourceText source code that originated the Node
+	 */
 	public BoxObjectAccess(BoxExpr context, BoxExpr access, Position position, String sourceText ) {
 		super( position, sourceText );
 		this.context = context;
