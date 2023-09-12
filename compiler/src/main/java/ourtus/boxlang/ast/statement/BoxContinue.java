0136e69c6ddd510e8602371dc6f5ed3870a9a743
mctaverna <maurizio.taverna@strumenta.com>
2023-09-12 10:50:53 +0200

    [compiler] Added documentation

--- compiler/src/main/java/ourtus/boxlang/ast/statement/BoxContinue.java
+++ compiler/src/main/java/ourtus/boxlang/ast/statement/BoxContinue.java
@@ -17,8 +17,16 @@ package ourtus.boxlang.ast.statement;
 import ourtus.boxlang.ast.BoxStatement;
 import ourtus.boxlang.ast.Position;
 
+/**
+ * AST Node representing a continue statement
+ */
 public class BoxContinue extends BoxStatement {
 
+	/**
+	 * Creates the AST node
+	 * @param position position of the statement in the source code
+	 * @param sourceText source code that originated the Node
+	 */
 	public BoxContinue(Position position, String sourceText) {
 		super(position, sourceText);
 	}
