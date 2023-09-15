9988e51307fa2165a4b62fae815945367a7af576
Luis Majano <lmajano@gmail.com>
2023-09-15 14:49:40 +0200

    - added javadocs - Changed direct HashMap to abstract Map for better encapsulation - Changed `public` constructors to `protected` since it's an abstract class

--- runtime/src/main/java/ortus/boxlang/runtime/types/Function.java
+++ runtime/src/main/java/ortus/boxlang/runtime/types/Function.java
@@ -31,6 +31,15 @@ import ortus.boxlang.runtime.scopes.Key;
  */
 public abstract class Function implements IType {
 
+	/**
+	 * --------------------------------------------------------------------------
+	 * Public Properties
+	 * --------------------------------------------------------------------------
+	 */
+
+	/**
+	 * The supported access levels of the function
+	 */
 	public enum Access {
 		PRIVATE,
 		PUBLIC,
@@ -38,54 +47,138 @@ public abstract class Function implements IType {
 		REMOTE
 	}
 
-	public static Key				ARGUMENT_COLLECTION	= Key.of( "argumentCollection" );
+	/**
+	 * The argument collection key which defaults to : {@code argumentCollection}
+	 */
+	public static Key			ARGUMENT_COLLECTION	= Key.of( "argumentCollection" );
+
+	/**
+	 * --------------------------------------------------------------------------
+	 * Private Properties
+	 * --------------------------------------------------------------------------
+	 */
 
 	/**
 	 * The arguments of the function
 	 */
-	private Argument[]				arguments;
+	private Argument[]			arguments;
 
 	/**
 	 * The name of the function
 	 */
-	private Key						name;
+	private Key					name;
 
 	/**
 	 * Additional abitrary metadata about this function.
 	 */
-	private HashMap<Key, Object>	metadata;
+	private Map<Key, Object>	metadata;
+
+	/**
+	 * --------------------------------------------------------------------------
+	 * Constructors
+	 * --------------------------------------------------------------------------
+	 */
 
 	/**
 	 * Constructor
+	 *
+	 * @param name      The name of the function
+	 * @param arguments The arguments of the function
+	 * @param metadata  Additional abitrary metadata about this function.
 	 */
-	public Function( Key name, Argument[] arguments, HashMap<Key, Object> metadata ) {
+	protected Function( Key name, Argument[] arguments, Map<Key, Object> metadata ) {
 		this.name		= name;
 		this.arguments	= arguments;
 		this.metadata	= metadata;
 	}
 
-	public Function( Key name, Argument[] arguments ) {
-		this( name, arguments, new HashMap<Key, Object>() );
+	/**
+	 * Constructor
+	 *
+	 * @param name      The name of the function
+	 * @param arguments The arguments of the function
+	 */
+	protected Function( Key name, Argument[] arguments ) {
+		this( name, arguments, new HashMap<>() );
 	}
 
-	public Function( Key name ) {
-		this( name, new Argument[] {}, new HashMap<Key, Object>() );
+	/**
+	 * Constructor
+	 *
+	 * @param name The name of the function
+	 */
+	protected Function( Key name ) {
+		this( name, new Argument[] {}, new HashMap<>() );
 	}
 
+	/**
+	 * --------------------------------------------------------------------------
+	 * Getters
+	 * --------------------------------------------------------------------------
+	 */
+
+	/**
+	 * Get the arguments of the function
+	 *
+	 * @return The arguments
+	 */
 	public Argument[] getArguments() {
 		return arguments;
 	}
 
+	/**
+	 * Get the name of the function
+	 *
+	 * @return The name
+	 */
 	public Key getName() {
 		return name;
 	}
 
-	public HashMap<Key, Object> getMetadata() {
+	/**
+	 * Get the metadata of the function
+	 *
+	 * @return The metadata
+	 */
+	public Map<Key, Object> getMetadata() {
 		return metadata;
 	}
 
+	/**
+	 * Return a string representation of the function
+	 */
+	public String asString() {
+		return toString();
+	}
+
+	/**
+	 * --------------------------------------------------------------------------
+	 * Invokers (These are the methods that are called by the runtime)
+	 * --------------------------------------------------------------------------
+	 */
+
+	/**
+	 * Implement this method to invoke the function
+	 *
+	 * @param context
+	 *
+	 * @return
+	 */
 	public abstract Object invoke( FunctionBoxContext context );
 
+	/**
+	 * --------------------------------------------------------------------------
+	 * Arguments Helpers
+	 * --------------------------------------------------------------------------
+	 */
+
+	/**
+	 * Create an arguments scope from the positional arguments
+	 *
+	 * @param positionalArguments The positional arguments
+	 *
+	 * @return The arguments scope
+	 */
 	public ArgumentsScope createArgumentsScope( Object[] positionalArguments ) {
 		Argument[]		arguments	= getArguments();
 		ArgumentsScope	scope		= new ArgumentsScope();
@@ -115,6 +208,13 @@ public abstract class Function implements IType {
 		return scope;
 	}
 
+	/**
+	 * Create an arguments scope from the named arguments
+	 *
+	 * @param namedArguments The named arguments
+	 *
+	 * @return The arguments scope
+	 */
 	public ArgumentsScope createArgumentsScope( Map<Key, Object> namedArguments ) {
 		Argument[]		arguments	= getArguments();
 		ArgumentsScope	scope		= new ArgumentsScope();
@@ -149,12 +249,13 @@ public abstract class Function implements IType {
 		return scope;
 	}
 
+	/**
+	 * Create an arguments scope from no arguments
+	 *
+	 * @return The arguments scope
+	 */
 	public ArgumentsScope createArgumentsScope() {
 		return createArgumentsScope( new Object[] {} );
-	}
-
-	public String asString() {
-		return toString();
 	}
 
 	/**
@@ -166,29 +267,25 @@ public abstract class Function implements IType {
 	 * @param defaultValue The default value of the argument
 	 *
 	 */
-	public record Argument( boolean required, String type, Key name, Object defaultValue, String hint, HashMap<Key, Object> metadata ) {
+	public record Argument( boolean required, String type, Key name, Object defaultValue, String hint, Map<Key, Object> metadata ) {
 
 		public Argument( Key name ) {
-			this( false, "any", name, null, "", new HashMap<Key, Object>() );
+			this( false, "any", name, null, "", new HashMap<>() );
 		}
 
 		public Argument( boolean required, String type, Key name ) {
-			this( required, type, name, null, "", new HashMap<Key, Object>() );
+			this( required, type, name, null, "", new HashMap<>() );
 		}
 
 		public Argument( boolean required, String type, Key name, Object defaultValue ) {
-			this( required, type, name, defaultValue, "", new HashMap<Key, Object>() );
+			this( required, type, name, defaultValue, "", new HashMap<>() );
 		}
 
 		public Argument( boolean required, String type, Key name, Object defaultValue, String hint ) {
-			this( required, type, name, defaultValue, hint, new HashMap<Key, Object>() );
+			this( required, type, name, defaultValue, hint, new HashMap<>() );
 		}
 
-		public Argument( boolean required, String type, Key name, Object defaultValue, HashMap<Key, Object> metadata ) {
-			this( required, type, name, defaultValue, "", metadata );
-		}
-
-		public Argument( boolean required, String type, Key name, Object defaultValue, String hint, HashMap<Key, Object> metadata ) {
+		public Argument( boolean required, String type, Key name, Object defaultValue, String hint, Map<Key, Object> metadata ) {
 			this.required		= required;
 			this.type			= type;
 			this.name			= name;
@@ -199,6 +296,17 @@ public abstract class Function implements IType {
 
 	}
 
+	/**
+	 * Ensure the argument is the correct type
+	 *
+	 * @param name  The name of the argument
+	 * @param value The value of the argument
+	 * @param type  The type of the argument
+	 *
+	 * @return The value of the argument
+	 *
+	 * @throws RuntimeException if the argument is not the correct type
+	 */
 	protected Object ensureArgumentType( Key name, Object value, String type ) {
 		CastAttempt<Object> typeCheck = GenericCaster.attempt( value, type, true );
 		if ( !typeCheck.wasSuccessful() ) {
