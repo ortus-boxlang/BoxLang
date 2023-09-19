44e5dff1a857d0e1fd0ac82fa82eb13f1c16bfd8
mctaverna <maurizio.taverna@strumenta.com>
2023-09-19 13:25:26 +0200

    [compiler] Added interpolation

---
+++ compiler/src/main/java/ortus/boxlang/transpiler/transformer/expression/BoxStringInterpolationTransformer.java
@@ -0,0 +1,39 @@
+
+package ortus.boxlang.transpiler.transformer.expression;
+
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.expr.Expression;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ortus.boxlang.ast.BoxNode;
+import ortus.boxlang.ast.expression.BoxStringInterpolation;
+import ortus.boxlang.transpiler.BoxLangTranspiler;
+import ortus.boxlang.transpiler.transformer.AbstractTransformer;
+import ortus.boxlang.transpiler.transformer.TransformerContext;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class BoxStringInterpolationTransformer extends AbstractTransformer  {
+	Logger logger = LoggerFactory.getLogger( BoxStringInterpolationTransformer.class );
+
+	@Override
+	public Node transform(BoxNode node, TransformerContext context) throws IllegalStateException {
+		BoxStringInterpolation interpolation = (BoxStringInterpolation) node;
+		String expr = interpolation.getValues()
+			.stream()
+			.map(it -> {
+				return BoxLangTranspiler.transform(it, TransformerContext.RIGHT).toString();
+
+			})
+			.collect(Collectors.joining("+"));
+		;
+
+		Node javaExpr = parseExpression(expr, new HashMap<>());
+		logger.info(node.getSourceText() + " -> " + javaExpr);
+		addIndex(javaExpr, node);
+		return javaExpr;
+	}
+}
