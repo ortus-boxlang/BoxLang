241f055e663be1e93c78c40cb81daad3635e711a
mctaverna <maurizio.taverna@strumenta.com>
2023-09-20 14:27:22 +0200

    [compiler] Do Statement

---
+++ compiler/src/main/java/ortus/boxlang/ast/statement/BoxDo.java
@@ -0,0 +1,55 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package ortus.boxlang.ast.statement;
+
+import ortus.boxlang.ast.BoxExpr;
+import ortus.boxlang.ast.BoxStatement;
+import ortus.boxlang.ast.Position;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * AST Node representing a Do loop statement
+ */
+public class BoxDo extends BoxStatement {
+
+	private final BoxExpr				condition;
+	private final List<BoxStatement>	body;
+
+	/**
+	 * Creates the AST node
+	 *
+	 * @param condition  the expression of the while statement
+	 * @param body       list of the statements in the body if the loop
+	 * @param position   position of the statement in the source code
+	 * @param sourceText source code that originated the Node
+	 */
+	public BoxDo( BoxExpr condition, List<BoxStatement> body, Position position, String sourceText ) {
+		super( position, sourceText );
+		this.condition = condition;
+		this.condition.setParent( this );
+		this.body = Collections.unmodifiableList( body );
+		this.body.forEach( arg -> arg.setParent( this ) );
+	}
+
+	public BoxExpr getCondition() {
+		return condition;
+	}
+
+	public List<BoxStatement> getBody() {
+		return body;
+	}
+}
---
+++ compiler/src/main/java/ortus/boxlang/transpiler/transformer/statement/BoxDoTransformer.java
@@ -0,0 +1,50 @@
+package ortus.boxlang.transpiler.transformer.statement;
+
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.expr.Expression;
+import com.github.javaparser.ast.stmt.BlockStmt;
+import com.github.javaparser.ast.stmt.DoStmt;
+import com.github.javaparser.ast.stmt.Statement;
+import com.github.javaparser.ast.stmt.WhileStmt;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ortus.boxlang.ast.BoxNode;
+import ortus.boxlang.ast.BoxStatement;
+import ortus.boxlang.ast.statement.BoxDo;
+import ortus.boxlang.ast.statement.BoxWhile;
+import ortus.boxlang.transpiler.BoxLangTranspiler;
+import ortus.boxlang.transpiler.transformer.AbstractTransformer;
+import ortus.boxlang.transpiler.transformer.TransformerContext;
+import ortus.boxlang.transpiler.transformer.expression.BoxParenthesisTransformer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BoxDoTransformer extends AbstractTransformer {
+
+	Logger logger = LoggerFactory.getLogger( BoxParenthesisTransformer.class );
+
+	@Override
+	public Node transform( BoxNode node, TransformerContext context ) throws IllegalStateException {
+		BoxDo		boxDo		= ( BoxDo ) node;
+		Expression	condition	= ( Expression ) BoxLangTranspiler.transform( boxDo.getCondition(), TransformerContext.RIGHT );
+
+		String		template	= "do  {} while(  ${condition}  );";
+		if ( requiresBooleanCaster( boxDo.getCondition() ) ) {
+			template = "do {} while( BooleanCaster.cast( ${condition} ) );";
+		}
+		Map<String, String>	values	= new HashMap<>() {
+
+										{
+											put( "condition", condition.toString() );
+										}
+									};
+		DoStmt				javaDo	= ( DoStmt ) parseStatement( template, values );
+		BlockStmt			body	= new BlockStmt();
+		for ( BoxStatement statement : boxDo.getBody() ) {
+			body.getStatements().add( ( Statement ) BoxLangTranspiler.transform( statement ) );
+		}
+		javaDo.setBody( body );
+		return javaDo;
+	}
+}
