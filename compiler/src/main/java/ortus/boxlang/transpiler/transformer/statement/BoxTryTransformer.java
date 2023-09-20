b79f7726559503aaa8931c8a88831b6fa9de71d4
mctaverna <maurizio.taverna@strumenta.com>
2023-09-20 14:34:00 +0200

    [compiler] Try/Catch

---
+++ compiler/src/main/java/ortus/boxlang/transpiler/transformer/statement/BoxTryTransformer.java
@@ -0,0 +1,99 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package ortus.boxlang.transpiler.transformer.statement;
+
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.body.Parameter;
+import com.github.javaparser.ast.expr.SimpleName;
+import com.github.javaparser.ast.stmt.BlockStmt;
+import com.github.javaparser.ast.stmt.CatchClause;
+import com.github.javaparser.ast.stmt.Statement;
+import com.github.javaparser.ast.stmt.TryStmt;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
+import com.github.javaparser.ast.type.Type;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ortus.boxlang.ast.BoxNode;
+import ortus.boxlang.ast.expression.BoxIdentifier;
+import ortus.boxlang.ast.expression.BoxStringLiteral;
+import ortus.boxlang.ast.statement.BoxTry;
+import ortus.boxlang.ast.statement.BoxTryCatch;
+import ortus.boxlang.transpiler.BoxLangTranspiler;
+import ortus.boxlang.transpiler.transformer.AbstractTransformer;
+import ortus.boxlang.transpiler.transformer.TransformerContext;
+import ortus.boxlang.transpiler.transformer.expression.BoxParenthesisTransformer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BoxTryTransformer extends AbstractTransformer {
+
+	Logger logger = LoggerFactory.getLogger( BoxParenthesisTransformer.class );
+
+	@Override
+	public Node transform( BoxNode node, TransformerContext context ) throws IllegalStateException {
+		BoxTry		boxTry	= ( BoxTry ) node;
+		TryStmt		javaTry	= new TryStmt();
+
+		BlockStmt	tryBody	= new BlockStmt();
+		boxTry.getTryBody().stream().forEach( stmt -> tryBody.getStatements().add(
+		    ( Statement ) BoxLangTranspiler.transform( stmt )
+		) );
+
+		NodeList<CatchClause> catchClauses = new NodeList<CatchClause>();
+		boxTry.getCatches().stream().forEach( clause -> {
+			BlockStmt catchBody = new BlockStmt();
+			clause.getCatchBody().stream().forEach( stmt -> catchBody.getStatements().add(
+			    ( Statement ) BoxLangTranspiler.transform( stmt )
+			) );
+			String				name	= computeName( clause );
+			Map<String, String>	values	= new HashMap<>() {
+
+											{
+												put( "expr", name );
+											}
+										};
+
+			Statement			handler	= ( Statement ) parseStatement( "catchContext = new CatchBoxContext( context, Key.of( \"${expr}\" ), ${expr} );",
+			    values );
+			catchBody.addStatement( handler );
+
+			catchClauses.add( new CatchClause( new Parameter( new ClassOrInterfaceType( "Throwable" ), name ), catchBody ) );
+		}
+		);
+
+		BlockStmt finallyBody = new BlockStmt();
+		boxTry.getFinallyBody().stream().forEach( stmt -> finallyBody.getStatements().add(
+		    ( Statement ) BoxLangTranspiler.transform( stmt )
+		) );
+
+		javaTry.setTryBlock( tryBody );
+		javaTry.setCatchClauses( catchClauses );
+		javaTry.setFinallyBlock( finallyBody );
+		return javaTry;
+	}
+
+	private String computeName( BoxTryCatch clause ) throws IllegalStateException {
+		if ( clause.getException() instanceof BoxStringLiteral literal ) {
+			return literal.getValue();
+		}
+		if ( clause.getException() instanceof BoxIdentifier identifier ) {
+			return identifier.getName();
+		}
+		throw new IllegalStateException( "invalid name" );
+	}
+}
