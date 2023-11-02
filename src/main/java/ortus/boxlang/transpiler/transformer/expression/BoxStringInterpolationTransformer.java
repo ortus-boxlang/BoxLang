fb1869aab2229ce124a2656b188f97a0b9af61fd
mctaverna <maurizio.taverna@strumenta.com>
2023-11-02 14:53:37 +0100

    [compiler] WIP BL-28 (String interpolation)

--- src/main/java/ortus/boxlang/transpiler/transformer/expression/BoxStringInterpolationTransformer.java
+++ src/main/java/ortus/boxlang/transpiler/transformer/expression/BoxStringInterpolationTransformer.java
@@ -2,6 +2,7 @@
 package ortus.boxlang.transpiler.transformer.expression;
 
 import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.expr.Expression;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ortus.boxlang.ast.BoxNode;
@@ -11,21 +12,44 @@ import ortus.boxlang.transpiler.transformer.AbstractTransformer;
 import ortus.boxlang.transpiler.transformer.TransformerContext;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Transform a String Interpolatiion the equivalent Java Parser AST nodes
+ */
 public class BoxStringInterpolationTransformer extends AbstractTransformer {
 
 	Logger logger = LoggerFactory.getLogger( BoxStringInterpolationTransformer.class );
 
+	/**
+	 * Transform a String interpolation expression
+	 * 
+	 * @param node    a BoxStringInterpolation
+	 * @param context transformation context
+	 * 
+	 * @return a Java Parser Expression
+	 * 
+	 * @throws IllegalStateException
+	 */
 	@Override
 	public Node transform( BoxNode node, TransformerContext context ) throws IllegalStateException {
 		BoxStringInterpolation	interpolation	= ( BoxStringInterpolation ) node;
-		String					expr			= interpolation.getValues()
+		List<Node>				operands		= interpolation.getValues()
 		    .stream()
-		    .map( it -> resolveScope( BoxLangTranspiler.transform( it, TransformerContext.RIGHT ), context ).toString() )
-		    .collect( Collectors.joining( "+" ) );
+		    .map( it -> resolveScope( BoxLangTranspiler.transform( it, TransformerContext.RIGHT ), context ) )
+		    .toList();
+		// .collect( Collectors.joining( "+" ) );
+		String					expr			= operands.get( operands.size() - 1 ).toString();
+		for ( int i = operands.size() - 1; i-- > 0; ) {
+			if ( i > 1 ) {
+				expr = "Concat.invoke(" + operands.get( i - 1 ) + "," + expr + ")";
+			} else {
+				expr = "Concat.invoke(" + operands.get( i ) + "," + expr + ")";
+			}
+		}
 
-		Node					javaExpr		= parseExpression( expr, new HashMap<>() );
+		Node javaExpr = parseExpression( expr.toString(), new HashMap<>() );
 		logger.info( "{} -> {}", node.getSourceText(), javaExpr );
 		addIndex( javaExpr, node );
 		return javaExpr;
