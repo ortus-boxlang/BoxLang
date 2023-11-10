e0785e55fe3955234e279c60a5982d85aefaedbc
Michael Born <michael@ortussolutions.com>
2023-11-10 10:27:53 -0500

    Use unsynchronized StringBuilder for faster MD5 generation

--- src/main/java/ortus/boxlang/runtime/runnables/compiler/JavaBoxpiler.java
+++ src/main/java/ortus/boxlang/runtime/runnables/compiler/JavaBoxpiler.java
@@ -435,7 +435,7 @@ public class JavaBoxpiler {
 		try {
 			java.security.MessageDigest	md		= java.security.MessageDigest.getInstance( "MD5" );
 			byte[]						array	= md.digest( md5.getBytes() );
-			StringBuffer				sb		= new StringBuffer();
+			StringBuilder sb					= new StringBuilder();
 			for ( int i = 0; i < array.length; ++i ) {
 				sb.append( Integer.toHexString( ( array[ i ] & 0xFF ) | 0x100 ).substring( 1, 3 ) );
 			}
