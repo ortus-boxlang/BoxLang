2025b17d6e3349df72f060e03d9a6577ae47991a
Jacob Beers <jacobtbeers@gmail.com>
2023-12-14 12:12:09 -0600

    Add tests for array BIFs

---
+++ src/test/java/ortus/boxlang/runtime/bifs/global/array/ArrayAppendTest.java
@@ -0,0 +1,87 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ortus.boxlang.runtime.bifs.global.array;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import ortus.boxlang.runtime.BoxRuntime;
+import ortus.boxlang.runtime.context.IBoxContext;
+import ortus.boxlang.runtime.context.ScriptingBoxContext;
+import ortus.boxlang.runtime.scopes.IScope;
+import ortus.boxlang.runtime.scopes.Key;
+import ortus.boxlang.runtime.scopes.VariablesScope;
+import ortus.boxlang.runtime.types.Array;
+
+public class ArrayAppendTest {
+
+    static BoxRuntime  instance;
+    static IBoxContext context;
+    static IScope      variables;
+    static Key         result = new Key( "result" );
+
+    @BeforeAll
+    public static void setUp() {
+        instance  = BoxRuntime.getInstance( true );
+        context   = new ScriptingBoxContext( instance.getRuntimeContext() );
+        variables = context.getScopeNearby( VariablesScope.name );
+    }
+
+    @AfterAll
+    public static void teardown() {
+        instance.shutdown();
+    }
+
+    @BeforeEach
+    public void setupEach() {
+        variables.clear();
+    }
+
+    @DisplayName( "It should increase the size of the array" )
+    @Test
+    public void testInreasesSIze() {
+        instance.executeSource(
+            """
+            arr = [ 1, 2, 3 ];
+            arrayAppend( arr, 4 );
+            result = arrayLen( arr );
+            """,
+            context );
+        assertThat( variables.dereference( result, false ) ).isEqualTo( 4 );
+    }
+
+    @DisplayName( "It should place the item at the end of the array" )
+    @Test
+    public void testAtEndOfArray() {
+        instance.executeSource(
+            """
+            arr = [ 1, 2, 3 ];
+            arrayAppend( arr, "test" );
+            result = arrayLen( arr );
+            """,
+            context );
+        assertThat( ( ( Array ) variables.dereference( Key.of( "arr" ), false ) ).get( 3 ) ).isEqualTo( "test" );
+    }
+
+}
---
+++ src/test/java/ortus/boxlang/runtime/bifs/global/array/ArrayLenTest.java
@@ -0,0 +1,72 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ortus.boxlang.runtime.bifs.global.array;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import ortus.boxlang.runtime.BoxRuntime;
+import ortus.boxlang.runtime.context.IBoxContext;
+import ortus.boxlang.runtime.context.ScriptingBoxContext;
+import ortus.boxlang.runtime.scopes.IScope;
+import ortus.boxlang.runtime.scopes.Key;
+import ortus.boxlang.runtime.scopes.VariablesScope;
+
+public class ArrayLenTest {
+
+    static BoxRuntime  instance;
+    static IBoxContext context;
+    static IScope      variables;
+    static Key         result = new Key( "result" );
+
+    @BeforeAll
+    public static void setUp() {
+        instance  = BoxRuntime.getInstance( true );
+        context   = new ScriptingBoxContext( instance.getRuntimeContext() );
+        variables = context.getScopeNearby( VariablesScope.name );
+    }
+
+    @AfterAll
+    public static void teardown() {
+        instance.shutdown();
+    }
+
+    @BeforeEach
+    public void setupEach() {
+        variables.clear();
+    }
+
+    @DisplayName( "It returns the length of the array" )
+    @Test
+    public void testItReturnsLength() {
+        instance.executeSource(
+            """
+            arr = [ 1, 2, 3 ];
+            result = arrayLen( arr );
+            """,
+            context );
+        assertThat( variables.dereference( result, false ) ).isEqualTo( 3 );
+    }
+
+}
