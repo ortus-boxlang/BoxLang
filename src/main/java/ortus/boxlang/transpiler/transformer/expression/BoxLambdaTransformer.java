075a78160f7c8c239f802a15554c9dbe3bf1242f
mctaverna <maurizio.taverna@strumenta.com>
2023-12-15 16:41:40 +0100

    Create transformer

---
+++ src/main/java/ortus/boxlang/transpiler/transformer/expression/BoxLambdaTransformer.java
@@ -0,0 +1,223 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package ortus.boxlang.transpiler.transformer.expression;
+
+import com.github.javaparser.ParseResult;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.expr.ArrayInitializerExpr;
+import com.github.javaparser.ast.expr.Expression;
+import com.github.javaparser.ast.stmt.BlockStmt;
+import com.github.javaparser.ast.stmt.Statement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ortus.boxlang.ast.BoxNode;
+import ortus.boxlang.ast.BoxStatement;
+import ortus.boxlang.ast.Source;
+import ortus.boxlang.ast.expression.BoxLambda;
+import ortus.boxlang.ast.statement.BoxFunctionDeclaration;
+import ortus.boxlang.runtime.config.util.PlaceholderHelper;
+import ortus.boxlang.runtime.types.exceptions.BoxRuntimeException;
+import ortus.boxlang.transpiler.JavaTranspiler;
+import ortus.boxlang.transpiler.transformer.AbstractTransformer;
+import ortus.boxlang.transpiler.transformer.Transformer;
+import ortus.boxlang.transpiler.transformer.TransformerContext;
+import ortus.boxlang.transpiler.transformer.statement.BoxFunctionDeclarationTransformer;
+
+import java.util.Map;
+
+/**
+ * Transform a Lambda in the equivalent Java Class
+ */
+public class BoxLambdaTransformer extends AbstractTransformer {
+
+	Logger			logger		= LoggerFactory.getLogger( BoxLambdaTransformer.class );
+	// @formatter:off
+	private String template = """
+		package ${packageName};
+
+		import ortus.boxlang.runtime.types.Argument;
+		import ortus.boxlang.runtime.types.Lambda;
+		import ortus.boxlang.runtime.types.Struct;
+		import ortus.boxlang.runtime.scopes.IScope;
+		import ortus.boxlang.runtime.scopes.Key;
+		import ortus.boxlang.runtime.context.FunctionBoxContext;
+		import ortus.boxlang.runtime.runnables.IBoxRunnable;
+		import ortus.boxlang.runtime.context.IBoxContext;
+		import ortus.boxlang.runtime.loader.ImportDefinition;
+
+		// Classes Auto-Imported on all Templates and Classes by BoxLang
+		import java.time.LocalDateTime;
+		import java.time.Instant;
+		import java.lang.System;
+		import java.lang.String;
+		import java.lang.Character;
+		import java.lang.Boolean;
+		import java.lang.Double;
+		import java.lang.Integer;
+		import java.util.*;
+		import java.nio.file.*;
+
+  		public class ${classname} extends Lambda {
+			private static ${classname}				instance;
+			private final static Key				name		= Key.of( "${lambdaName}" );
+			private final static Argument[]			arguments	= new Argument[] {};
+			private final static String				returnType	= "any";
+
+		    private final static Struct			annotations			= Struct.EMPTY;
+			private final static Struct			documentation		= Struct.EMPTY;
+
+
+			public Key getName() {
+				return name;
+			}
+			public Argument[] getArguments() {
+				return arguments;
+			}
+			public String getReturnType() {
+				return returnType;
+			}
+
+			public Access getAccess() {
+   				return Access.PUBLIC;
+   			}
+
+			public String getHint() {
+				return "";
+			}
+
+   			public  Map<Key, Object> getAdditionalMetadata() {
+   				return null;
+   			}
+
+			public  long getRunnableCompileVersion() {
+				return 0L;
+			}
+
+			public LocalDateTime getRunnableCompiledOn() {
+				return null;
+			}
+
+			public IBoxRunnable getDeclaringRunnable() {
+				return null;
+			}
+
+			public Object getRunnableAST() {
+				return null;
+			}
+
+			private ${classname}() {
+				super();
+			}
+
+			public static synchronized ${classname} getInstance() {
+				if ( instance == null ) {
+					instance = new ${classname}();
+				}
+				return instance;
+			}
+
+			@Override
+      			public List<ImportDefinition> getImports() {
+      			return null;
+      		}
+
+			@Override
+			public Struct getAnnotations() {
+				return annotations;
+			}
+
+			@Override
+			public Struct getDocumentation() {
+				return documentation;
+			}
+			@Override
+			public Object _invoke( FunctionBoxContext context ) {
+
+			}
+
+  		}
+ 	""";
+	// @formatter:on
+	public BoxLambdaTransformer( JavaTranspiler transpiler ) {
+		super( transpiler );
+	}
+
+	/**
+	 * Transform a lambda declaration into a Class
+	 *
+	 * @param node
+	 * @param context
+	 *
+	 * @return
+	 *
+	 * @throws IllegalStateException
+	 */
+	@Override
+	public Node transform( BoxNode node, TransformerContext context ) throws IllegalStateException {
+		BoxLambda			boxLambda	= ( BoxLambda ) node;
+		Source				source		= boxLambda.getPosition().getSource();
+		String				packageName	= JavaTranspiler.getPackageName( source );
+		String				lambdaName	= "$lambda" + transpiler.incrementAndGetLambdaCounter();
+		String				className	= JavaTranspiler.getClassName( source ) + lambdaName;
+
+		Map<String, String>	values		= Map.ofEntries(
+		    Map.entry( "packageName", packageName ),
+		    Map.entry( "className", className ),
+		    Map.entry( "lambdaName", lambdaName )
+		);
+		transpiler.pushContextName( "context" );
+		String							code	= PlaceholderHelper.resolve( template, values );
+		ParseResult<CompilationUnit>	result;
+		try {
+			result = javaParser.parse( code );
+		} catch ( Exception e ) {
+			// Temp debugging to see generated Java code
+			throw new BoxRuntimeException( code, e );
+		}
+		if ( !result.isSuccessful() ) {
+			// Temp debugging to see generated Java code
+			throw new BoxRuntimeException( result + "\n" + code );
+		}
+		CompilationUnit			javaClass		= result.getResult().get();
+		/* Transform the arguments creating the initialization values */
+		ArrayInitializerExpr	argInitializer	= new ArrayInitializerExpr();
+		boxLambda.getArgs().forEach( arg -> {
+			Expression argument = ( Expression ) transpiler.transform( arg );
+			argInitializer.getValues().add( argument );
+		} );
+		javaClass.getType( 0 ).getFieldByName( "arguments" ).orElseThrow().getVariable( 0 ).setInitializer( argInitializer );
+
+		/* Transform the annotations creating the initialization value */
+		Expression annotationStruct = transformAnnotations( boxLambda.getAnnotations() );
+		result.getResult().orElseThrow().getType( 0 ).getFieldByName( "annotations" ).orElseThrow().getVariable( 0 ).setInitializer( annotationStruct );
+
+		MethodDeclaration invokeMethod = javaClass.findCompilationUnit().orElseThrow()
+		    .getClassByName( className ).orElseThrow()
+		    .getMethodsByName( "_invoke" ).get( 0 );
+
+		for ( BoxStatement statement : boxLambda.getBody() ) {
+			Node javaStmt = transpiler.transform( statement );
+			if ( javaStmt instanceof BlockStmt stmt ) {
+				stmt.getStatements().forEach( it -> invokeMethod.getBody().get().addStatement( it ) );
+			} else {
+				invokeMethod.getBody().get().addStatement( ( Statement ) javaStmt );
+			}
+		}
+		transpiler.popContextName();
+		return javaClass;
+	}
+}
