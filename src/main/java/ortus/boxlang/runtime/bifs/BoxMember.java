8799f41de8be4b023f1fec1b3b1bc17aecee9545
Luis Majano <lmajano@gmail.com>
2023-12-19 12:10:26 +0100

    added license header

--- src/main/java/ortus/boxlang/runtime/bifs/BoxMember.java
+++ src/main/java/ortus/boxlang/runtime/bifs/BoxMember.java
@@ -1,3 +1,20 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ortus.boxlang.runtime.bifs;
 
 import java.lang.annotation.ElementType;
@@ -13,11 +30,11 @@ import ortus.boxlang.runtime.types.BoxLangType;
 @Repeatable( BoxMembers.class )
 public @interface BoxMember {
 
-    BoxLangType type();
+	BoxLangType type();
 
-    // If not provided, the name will be the name of the BIF with the BoxType replaced. So arrayAppend() would be append()
-    String name() default "";
+	// If not provided, the name will be the name of the BIF with the BoxType replaced. So arrayAppend() would be append()
+	String name() default "";
 
-    // If not provided, the argument will be the first argument of the BIF
-    String objectArgument() default "";
+	// If not provided, the argument will be the first argument of the BIF
+	String objectArgument() default "";
 }
