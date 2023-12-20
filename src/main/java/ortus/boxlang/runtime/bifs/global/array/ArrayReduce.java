d99f1850fc563014fb889122e163641cfd60d7f9
Luis Majano <lmajano@gmail.com>
2023-12-20 12:25:24 +0100

    dumb formatter again

--- src/main/java/ortus/boxlang/runtime/bifs/global/array/ArrayReduce.java
+++ src/main/java/ortus/boxlang/runtime/bifs/global/array/ArrayReduce.java
@@ -30,40 +30,40 @@ import ortus.boxlang.runtime.types.Function;
 @BoxMember( type = BoxLangType.ARRAY )
 public class ArrayReduce extends BIF {
 
-    /**
-     * Constructor
-     */
-    public ArrayReduce() {
-        super();
-        declaredArguments = new Argument[] {
-            new Argument( true, "array", Key.array ),
-            new Argument( true, "function", Key.callback ),
-            new Argument( Key.initialValue )
-        };
-    }
+	/**
+	 * Constructor
+	 */
+	public ArrayReduce() {
+		super();
+		declaredArguments = new Argument[] {
+		    new Argument( true, "array", Key.array ),
+		    new Argument( true, "function", Key.callback ),
+		    new Argument( Key.initialValue )
+		};
+	}
 
-    /**
-     * Run the provided udf over the array to reduce the values to a single output
-     *
-     * @param context   The context in which the BIF is being invoked.
-     * @param arguments Argument scope for the BIF.
-     * 
-     * @argument.array The array to reduce
-     * 
-     * @argument.callback The function to invoke for each item. The function will be passed 3 arguments: the accumulator, the current item, and the
-     *                    current index. The function should return the new accumulator value.
-     * 
-     * @argument.initialValue The initial value of the accumulator
-     */
-    public Object invoke( IBoxContext context, ArgumentsScope arguments ) {
-        Array    actualArray = ArrayCaster.cast( arguments.dereference( Key.array, false ) );
-        Object   accumulator = arguments.get( Key.initialValue );
-        Function func        = ( Function ) arguments.get( Key.callback );
+	/**
+	 * Run the provided udf over the array to reduce the values to a single output
+	 *
+	 * @param context   The context in which the BIF is being invoked.
+	 * @param arguments Argument scope for the BIF.
+	 *
+	 * @argument.array The array to reduce
+	 *
+	 * @argument.callback The function to invoke for each item. The function will be passed 3 arguments: the accumulator, the current item, and the
+	 *                    current index. The function should return the new accumulator value.
+	 *
+	 * @argument.initialValue The initial value of the accumulator
+	 */
+	public Object invoke( IBoxContext context, ArgumentsScope arguments ) {
+		Array		actualArray	= ArrayCaster.cast( arguments.dereference( Key.array, false ) );
+		Object		accumulator	= arguments.get( Key.initialValue );
+		Function	func		= ( Function ) arguments.get( Key.callback );
 
-        for ( int i = 0; i < actualArray.size(); i++ ) {
-            accumulator = context.invokeFunction( func, new Object[] { accumulator, actualArray.get( i ), i + 1, actualArray } );
-        }
+		for ( int i = 0; i < actualArray.size(); i++ ) {
+			accumulator = context.invokeFunction( func, new Object[] { accumulator, actualArray.get( i ), i + 1, actualArray } );
+		}
 
-        return accumulator;
-    }
+		return accumulator;
+	}
 }
