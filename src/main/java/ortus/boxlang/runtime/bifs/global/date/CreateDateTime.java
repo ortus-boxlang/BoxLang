64ccfeb2a0ac63076ff33bd3d0cf6a31b4af481d
jclausen <jclausen@ortussolutions.com>
2023-12-31 16:02:41 -0500

    Add CreateDateTime BIF

---
+++ src/main/java/ortus/boxlang/runtime/bifs/global/date/CreateDateTime.java
@@ -0,0 +1,55 @@
+
+package ortus.boxlang.runtime.bifs.global.date;
+
+import ortus.boxlang.runtime.bifs.BIF;
+import ortus.boxlang.runtime.bifs.BoxBIF;
+import ortus.boxlang.runtime.bifs.BoxMember;
+import ortus.boxlang.runtime.context.IBoxContext;
+import ortus.boxlang.runtime.scopes.ArgumentsScope;
+import ortus.boxlang.runtime.scopes.Key;
+import ortus.boxlang.runtime.types.Argument;
+import ortus.boxlang.runtime.types.BoxLangType;
+import ortus.boxlang.runtime.types.DateTime;
+
+@BoxBIF
+@BoxMember( type = BoxLangType.DATETIME )
+
+public class CreateDateTime extends BIF {
+
+	/**
+	 * Constructor
+	 */
+	public CreateDateTime() {
+		super();
+		declaredArguments = new Argument[] {
+		    new Argument( true, "integer", Key.of( "year" ) ),
+		    new Argument( true, "integer", Key.of( "month" ) ),
+		    new Argument( true, "integer", Key.of( "day" ) ),
+		    new Argument( true, "integer", Key.of( "hour" ) ),
+		    new Argument( true, "integer", Key.of( "minute" ) ),
+		    new Argument( true, "integer", Key.of( "second" ) ),
+		    new Argument( true, "integer", Key.of( "millisecond" ) )
+		};
+	}
+
+	/**
+	 * Describe what the invocation of your bif function does
+	 *
+	 * @param context   The context in which the BIF is being invoked.
+	 * @param arguments Argument scope for the BIF.
+	 *
+	 * @argument.foo Describe any expected arguments
+	 */
+	public Object invoke( IBoxContext context, ArgumentsScope arguments ) {
+		return new DateTime(
+		    ( int ) arguments.get( Key.of( "year" ) ),
+		    ( int ) arguments.get( Key.of( "month" ) ),
+		    ( int ) arguments.get( Key.of( "day" ) ),
+		    ( int ) arguments.get( Key.of( "hour" ) ),
+		    ( int ) arguments.get( Key.of( "minute" ) ),
+		    ( int ) arguments.get( Key.of( "second" ) ),
+		    ( int ) arguments.get( Key.of( "millisecond" ) )
+		);
+	}
+
+}
---
+++ src/test/java/ortus/boxlang/runtime/bifs/global/date/CreateDateTimeTest.java
@@ -0,0 +1,91 @@
+
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ortus.boxlang.runtime.bifs.global.date;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.junit.Ignore;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import ortus.boxlang.runtime.BoxRuntime;
+import ortus.boxlang.runtime.context.IBoxContext;
+import ortus.boxlang.runtime.context.ScriptingBoxContext;
+import ortus.boxlang.runtime.dynamic.casters.IntegerCaster;
+import ortus.boxlang.runtime.scopes.IScope;
+import ortus.boxlang.runtime.scopes.Key;
+import ortus.boxlang.runtime.scopes.VariablesScope;
+import ortus.boxlang.runtime.types.DateTime;
+
+public class CreateDateTimeTest {
+
+	static BoxRuntime	instance;
+	static IBoxContext	context;
+	static IScope		variables;
+	static Key			result	= new Key( "result" );
+
+	@BeforeAll
+	public static void setUp() {
+		instance	= BoxRuntime.getInstance( true );
+		context		= new ScriptingBoxContext( instance.getRuntimeContext() );
+		variables	= context.getScopeNearby( VariablesScope.name );
+	}
+
+	@AfterAll
+	public static void teardown() {
+		instance.shutdown();
+	}
+
+	@BeforeEach
+	public void setupEach() {
+		variables.clear();
+	}
+
+	@DisplayName( "It tests the BIF CreateDateTime" )
+	@Test
+	@Ignore
+	public void testBif() {
+		instance.executeSource(
+		    """
+		    result = createDateTime( 2023, 12, 31, 12, 30, 30, 0 );
+		    """,
+		    context );
+		DateTime result = ( DateTime ) variables.dereference( Key.of( "result" ), false );
+		assertThat( result ).isInstanceOf( DateTime.class );
+		assertThat( result.toString() ).isInstanceOf( String.class );
+		assertThat( IntegerCaster.cast( result.format( "yyyy" ) ) ).isEqualTo( 2023 );
+		assertThat( IntegerCaster.cast( result.format( "M" ) ) ).isEqualTo( 12 );
+		assertThat( IntegerCaster.cast( result.format( "d" ) ) ).isEqualTo( 31 );
+		assertThat( IntegerCaster.cast( result.format( "H" ) ) ).isEqualTo( 12 );
+		assertThat( IntegerCaster.cast( result.format( "m" ) ) ).isEqualTo( 30 );
+		assertThat( IntegerCaster.cast( result.format( "s" ) ) ).isEqualTo( 30 );
+		assertThat( IntegerCaster.cast( result.format( "n" ) ) ).isEqualTo( 0 );
+
+	}
+
+}
