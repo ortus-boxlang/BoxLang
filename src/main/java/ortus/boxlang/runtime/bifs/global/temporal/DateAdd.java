dc6f92d2efe67c8e06f137979d241424047c15d1
jclausen <jclausen@ortussolutions.com>
2023-12-31 19:09:51 -0500

    Add DateAdd BIF and member function

---
+++ src/main/java/ortus/boxlang/runtime/bifs/global/temporal/DateAdd.java
@@ -0,0 +1,54 @@
+
+package ortus.boxlang.runtime.bifs.global.temporal;
+
+import ortus.boxlang.runtime.bifs.BIF;
+import ortus.boxlang.runtime.bifs.BoxBIF;
+import ortus.boxlang.runtime.bifs.BoxMember;
+import ortus.boxlang.runtime.context.IBoxContext;
+import ortus.boxlang.runtime.scopes.ArgumentsScope;
+import ortus.boxlang.runtime.scopes.Key;
+import ortus.boxlang.runtime.types.Argument;
+import ortus.boxlang.runtime.types.BoxLangType;
+import ortus.boxlang.runtime.types.DateTime;
+
+@BoxBIF
+@BoxMember( type = BoxLangType.DATETIME, name = "add", objectArgument = "date" )
+
+public class DateAdd extends BIF {
+
+	/**
+	 * Constructor
+	 */
+	public DateAdd() {
+		super();
+		declaredArguments = new Argument[] {
+			new Argument( true, "string", Key.of( "datepart" ) ),
+			new Argument( true, "long", Key.of( "number" ) ),
+			new Argument( true, "any", Key.of( "date" ) )
+		};
+	}
+
+	/**
+	 * Modifies a date object by date part and integer time unit
+	 *
+	 * @param context   The context in which the BIF is being invoked.
+	 * @param arguments Argument scope for the BIF.
+	 *
+	 * @argument.foo Describe any expected arguments
+	 */
+	public Object invoke( IBoxContext context, ArgumentsScope arguments ) {
+		DateTime ref = null;
+		try {
+			ref = ( DateTime ) arguments.get( Key.of( "date" ) );
+		} catch( java.lang.ClassCastException e ){
+			ref = new DateTime( ( String ) arguments.get( Key.of( "date" ) ) );
+		} catch( Exception e ){
+			throw new RuntimeException( e );
+		}
+		return ref.modify(
+			( String ) arguments.get( Key.of( "datepart" ) ),
+			( Long ) arguments.get( Key.of( "number" ) )
+		);
+	}
+
+}
---
+++ src/test/java/ortus/boxlang/runtime/bifs/global/temporal/DateAddTest.java
@@ -0,0 +1,115 @@
+
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ortus.boxlang.runtime.bifs.global.temporal;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import ortus.boxlang.runtime.BoxRuntime;
+import ortus.boxlang.runtime.context.IBoxContext;
+import ortus.boxlang.runtime.context.ScriptingBoxContext;
+import ortus.boxlang.runtime.dynamic.casters.IntegerCaster;
+import ortus.boxlang.runtime.scopes.IScope;
+import ortus.boxlang.runtime.scopes.Key;
+import ortus.boxlang.runtime.scopes.VariablesScope;
+import ortus.boxlang.runtime.types.DateTime;
+
+public class DateAddTest {
+
+	static BoxRuntime	instance;
+	static IBoxContext	context;
+	static IScope		variables;
+	static Key			result	= new Key( "result" );
+
+	@BeforeAll
+	public static void setUp() {
+		instance	= BoxRuntime.getInstance( true );
+		context		= new ScriptingBoxContext( instance.getRuntimeContext() );
+		variables	= context.getScopeNearby( VariablesScope.name );
+	}
+
+	@AfterAll
+	public static void teardown() {
+		instance.shutdown();
+	}
+
+	@BeforeEach
+	public void setupEach() {
+		variables.clear();
+	}
+
+	@DisplayName( "It tests the BIF DateAdd" )
+	@Test
+	public void testBif() {
+		instance.executeSource(
+		    """
+		    result = dateAdd( "d", 1, createDate( 2023, 12, 31 ) );
+		    """,
+		    context );
+		DateTime result = ( DateTime ) variables.dereference( Key.of( "result" ), false );
+		assertThat( result ).isInstanceOf( DateTime.class );
+		assertThat( result.toString() ).isInstanceOf( String.class );
+		assertThat( IntegerCaster.cast( result.format( "yyyy" ) ) ).isEqualTo( 2024 );
+		assertThat( IntegerCaster.cast( result.format( "M" ) ) ).isEqualTo( 1 );
+		assertThat( IntegerCaster.cast( result.format( "d" ) ) ).isEqualTo( 1 );
+
+	}
+
+	@DisplayName( "It tests the BIF DateAdd with a string as the date argument" )
+	@Test
+	public void testBifWithDateString() {
+		instance.executeSource(
+		    """
+		    result = dateAdd( "d", 1, "2023-12-31" );
+		    """,
+		    context );
+		DateTime result = ( DateTime ) variables.dereference( Key.of( "result" ), false );
+		assertThat( result ).isInstanceOf( DateTime.class );
+		assertThat( result.toString() ).isInstanceOf( String.class );
+		assertThat( IntegerCaster.cast( result.format( "yyyy" ) ) ).isEqualTo( 2024 );
+		assertThat( IntegerCaster.cast( result.format( "M" ) ) ).isEqualTo( 1 );
+		assertThat( IntegerCaster.cast( result.format( "d" ) ) ).isEqualTo( 1 );
+
+	}
+
+	@DisplayName( "It tests the member function DateTime.add( datepart, number )" )
+	@Test
+	public void testMemberFunction() {
+		instance.executeSource(
+		    """
+			ref = createDate( 2023, 12, 31 )
+		    result = ref.add( datepart="d", number=1 );
+		    """,
+		    context );
+		DateTime result = ( DateTime ) variables.dereference( Key.of( "result" ), false );
+		assertThat( result ).isInstanceOf( DateTime.class );
+		assertThat( result.toString() ).isInstanceOf( String.class );
+		assertThat( IntegerCaster.cast( result.format( "yyyy" ) ) ).isEqualTo( 2024 );
+		assertThat( IntegerCaster.cast( result.format( "M" ) ) ).isEqualTo( 1 );
+		assertThat( IntegerCaster.cast( result.format( "d" ) ) ).isEqualTo( 1 );
+
+	}
+
+}
