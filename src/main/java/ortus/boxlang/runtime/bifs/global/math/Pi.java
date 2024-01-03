fa6c8f94ca58b7eb133749fd75f58431be263f35
Jacob Beers <jacobtbeers@gmail.com>
2024-01-02 21:38:38 -0600

    Baked a BIF Pi

---
+++ src/main/java/ortus/boxlang/runtime/bifs/global/math/Pi.java
@@ -0,0 +1,36 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package ortus.boxlang.runtime.bifs.global.math;
+
+import ortus.boxlang.runtime.bifs.BIF;
+import ortus.boxlang.runtime.bifs.BoxBIF;
+import ortus.boxlang.runtime.context.IBoxContext;
+import ortus.boxlang.runtime.scopes.ArgumentsScope;
+
+@BoxBIF
+public class Pi extends BIF {
+
+	/**
+	 * 
+	 * Returns the mathmatical constant Pi accurate to 15 digits
+	 * 
+	 * @param context   The context in which the BIF is being invoked.
+	 * @param arguments Argument scope for the BIF.
+	 * 
+	 */
+	public Double invoke( IBoxContext context, ArgumentsScope arguments ) {
+		return 3.14159265359;
+	}
+}
---
+++ src/test/java/ortus/boxlang/runtime/bifs/global/math/PiTest.java
@@ -0,0 +1,82 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ortus.boxlang.runtime.bifs.global.math;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import ortus.boxlang.runtime.BoxRuntime;
+import ortus.boxlang.runtime.context.IBoxContext;
+import ortus.boxlang.runtime.context.ScriptingBoxContext;
+import ortus.boxlang.runtime.scopes.IScope;
+import ortus.boxlang.runtime.scopes.Key;
+import ortus.boxlang.runtime.scopes.VariablesScope;
+
+public class PiTest {
+
+	static BoxRuntime	instance;
+	static IBoxContext	context;
+	static IScope		variables;
+	static Key			result	= new Key( "result" );
+
+	@BeforeAll
+	public static void setUp() {
+		instance	= BoxRuntime.getInstance( true );
+		context		= new ScriptingBoxContext( instance.getRuntimeContext() );
+		variables	= context.getScopeNearby( VariablesScope.name );
+	}
+
+	@AfterAll
+	public static void teardown() {
+		instance.shutdown();
+	}
+
+	@BeforeEach
+	public void setupEach() {
+		variables.clear();
+	}
+
+	@DisplayName( "It returns pi" )
+	@Test
+	public void testItReturnsMin() {
+		instance.executeSource(
+		    """
+		    result = pi();
+		    """,
+		    context );
+		assertThat( variables.get( result ) ).isEqualTo( 3.14159265359 );
+	}
+
+	@DisplayName( "It can correctly calculate the circumference of a 16in deep dish pizza" )
+	@Test
+	public void testPizzaCircumference() {
+		instance.executeSource(
+		    """
+		    result = pi() * 16;
+		    """,
+		    context );
+		assertThat( Math.round( ( ( Double ) variables.get( result ) ) * 10000d ) / 10000d ).isEqualTo( 50.2655 );
+	}
+
+}
