34fbb4a7b69a56b888669f6cc8bb524e1bfa439f
jclausen <jclausen@ortussolutions.com>
2024-01-03 15:30:12 -0500

    Add FileDelete BIF

---
+++ src/main/java/ortus/boxlang/runtime/bifs/global/io/FileDelete.java
@@ -0,0 +1,45 @@
+
+package ortus.boxlang.runtime.bifs.global.io;
+
+import java.io.IOException;
+
+import ortus.boxlang.runtime.bifs.BIF;
+import ortus.boxlang.runtime.bifs.BoxBIF;
+import ortus.boxlang.runtime.context.IBoxContext;
+import ortus.boxlang.runtime.scopes.ArgumentsScope;
+import ortus.boxlang.runtime.scopes.Key;
+import ortus.boxlang.runtime.types.Argument;
+import ortus.boxlang.runtime.util.FileSystemUtil;
+
+@BoxBIF
+
+public class FileDelete extends BIF {
+
+	/**
+	 * Constructor
+	 */
+	public FileDelete() {
+		super();
+		declaredArguments = new Argument[] {
+		    new Argument( true, "string", Key.file )
+		};
+	}
+
+	/**
+	 * Describe what the invocation of your bif function does
+	 *
+	 * @param context   The context in which the BIF is being invoked.
+	 * @param arguments Argument scope for the BIF.
+	 *
+	 * @argument.foo Describe any expected arguments
+	 */
+	public Object invoke( IBoxContext context, ArgumentsScope arguments ) {
+		try {
+			FileSystemUtil.deleteFile( arguments.getAsString( Key.file ) );
+		} catch ( IOException e ) {
+			throw new RuntimeException( e );
+		}
+		return null;
+	}
+
+}
