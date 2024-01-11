2ec72f6409102a2df03e926232f2eeaa7f6c9985
Grant Copley <gcopley@ortussolutions.com>
2024-01-11 14:35:52 -0600

    Initial implementation for aSin( number ) and number.aSin()

---
+++ src/main/java/ortus/boxlang/runtime/bifs/global/math/ASin.java
@@ -0,0 +1,55 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package ortus.boxlang.runtime.bifs.global.math;
+
+import ortus.boxlang.runtime.bifs.BIF;
+import ortus.boxlang.runtime.bifs.BoxBIF;
+import ortus.boxlang.runtime.bifs.BoxMember;
+import ortus.boxlang.runtime.context.IBoxContext;
+import ortus.boxlang.runtime.scopes.ArgumentsScope;
+import ortus.boxlang.runtime.scopes.Key;
+import ortus.boxlang.runtime.types.Argument;
+import ortus.boxlang.runtime.types.BoxLangType;
+
+@BoxBIF
+@BoxMember( type = BoxLangType.NUMERIC )
+public class ASin extends BIF {
+
+	/**
+	 * Constructor
+	 */
+	public ASin() {
+		super();
+		declaredArguments = new Argument[] {
+		    new Argument( true, "numeric", Key.number )
+		};
+	}
+
+	/**
+	 * Returns the arcsine (inverse sine) of a number
+	 *
+	 * @param context   The context in which the BIF is being invoked.
+	 * @param arguments Argument scope for the BIF.
+	 *
+	 * @argument.number The number to calculate the arcsine of
+	 */
+	public Object invoke( IBoxContext context, ArgumentsScope arguments ) {
+		double value = arguments.getAsDouble( Key.number );
+		if ( value < -1.0 || value > 1.0 ) {
+			throw new IllegalArgumentException( "Input value must be in the range [-1, 1] for ASin function." );
+		}
+		return Math.asin( value );
+	}
+}
