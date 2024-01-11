0acf220c76b874b165216406ab6d2aa0b4385a7d
Brad Wood <brad@bradwood.com>
2024-01-10 23:55:31 -0600

    Stupid find and replace..

--- src/main/java/ortus/boxlang/web/scopes/CGIScope.java
+++ src/main/java/ortus/boxlang/web/scopes/CGIScope.java
@@ -1,30 +1,29 @@
 /**
  * [BoxLang]
  *
- * Copyright [2023] [Ortus Solutions
- * Corp]
+ * Copyright [2023] [Ortus Solutions, Corp]
  *
- * Licensed under the Apache License
- * Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing
- * software
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND
- * either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 package ortus.boxlang.web.scopes;
 
 import io.undertow.server.HttpServerExchange;
+import io.undertow.util.HeaderValues;
 import ortus.boxlang.runtime.context.IBoxContext;
 import ortus.boxlang.runtime.scopes.BaseScope;
 import ortus.boxlang.runtime.scopes.Key;
+import ortus.boxlang.runtime.types.exceptions.BoxRuntimeException;
+import ortus.boxlang.runtime.types.meta.BoxMeta;
 
 /**
  * Variables scope implementation in BoxLang
@@ -64,15 +63,12 @@ public class CGIScope extends BaseScope {
 	 * @param value The value to assign
 	 */
 	@Override
-	public Object assign( IBoxContext context
-Key key
-Object value ) {
+	public Object assign( IBoxContext context, Key key, Object value ) {
 		throw new BoxRuntimeException( "Cannot assign to the CGI scope" );
 	}
 
 	/**
-	 * Dereference this object by a key and return the value
-or throw exception
+	 * Dereference this object by a key and return the value, or throw exception
 	 *
 	 * @param key  The key to dereference
 	 * @param safe Whether to throw an exception if the key is not found
@@ -80,9 +76,7 @@ or throw exception
 	 * @return The requested object
 	 */
 	@Override
-	public Object dereference( IBoxContext context
-Key key
-Boolean safe ) {
+	public Object dereference( IBoxContext context, Key key, Boolean safe ) {
 		// Special check for $bx
 		if ( key.equals( BoxMeta.key ) ) {
 			return getBoxMeta();
@@ -92,54 +86,56 @@ Boolean safe ) {
 			return exchange.getRequestPath();
 		}
 		// TODO: All other CGI keys
-/* 
-auth_password
-auth_typeauth_user
-cert_cookie
-cert_flags
-cert_issuer
-cert_keysize
-cert_secretkeysize
-cert_serialnumber
-cert_server_issuer
-cert_server_subject
-cert_subject
-cf_template_path
-content_length
-content_type
-context_path
-gateway_interface
-http_accept
-http_accept_encoding
-http_accept_language
-http_connection
-http_cookie
-http_host
-http_referer
-http_user_agent
-https_keysize
-https_secretkeysize
-https_server_issuer
-https_server_subject
-https,
-local_addr
-local_host 
-path_info
-path_translated
-query_string
-remote_addr
-remote_host
-remote_user
-request_method
-request_url
-script_name
-server_name
-server_port
-server_port_secure
-server_protocol
-server_software
-web_server_api
- */
+
+		/*
+		 * auth_password
+		 * auth_typeauth_user
+		 * cert_cookie
+		 * cert_flags
+		 * cert_issuer
+		 * cert_keysize
+		 * cert_secretkeysize
+		 * cert_serialnumber
+		 * cert_server_issuer
+		 * cert_server_subject
+		 * cert_subject
+		 * cf_template_path
+		 * content_length
+		 * content_type
+		 * context_path
+		 * gateway_interface
+		 * http_accept
+		 * http_accept_encoding
+		 * http_accept_language
+		 * http_connection
+		 * http_cookie
+		 * http_host
+		 * http_referer
+		 * http_user_agent
+		 * https_keysize
+		 * https_secretkeysize
+		 * https_server_issuer
+		 * https_server_subject
+		 * https,
+		 * local_addr
+		 * local_host
+		 * path_info
+		 * path_translated
+		 * query_string
+		 * remote_addr
+		 * remote_host
+		 * remote_user
+		 * request_method
+		 * request_url
+		 * script_name
+		 * server_name
+		 * server_port
+		 * server_port_secure
+		 * server_protocol
+		 * server_software
+		 * web_server_api
+		 */
+
 		// HTTP header fallbacks
 		HeaderValues header = exchange.getRequestHeaders().get( key.getName() );
 		if ( header != null ) {
