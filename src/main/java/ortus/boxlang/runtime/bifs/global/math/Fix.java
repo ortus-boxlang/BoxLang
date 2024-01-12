3281c5549b02e7ad52aab9d4cf1475a4dc0a5baf
Grant Copley <gcopley@ortussolutions.com>
2024-01-12 17:28:34 -0600

    Initial implementation of fix( number ) and number.fix()

---
+++ src/main/java/ortus/boxlang/runtime/bifs/global/math/Fix.java
@@ -0,0 +1,55 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package ortus.boxlang.runtime.bifs.global.math;
+
+import ortus.boxlang.runtime.bifs.BIF;
+import ortus.boxlang.runtime.bifs.BoxBIF;
+import ortus.boxlang.runtime.bifs.BoxMember;
+import ortus.boxlang.runtime.context.IBoxContext;
+import ortus.boxlang.runtime.scopes.ArgumentsScope;
+import ortus.boxlang.runtime.scopes.Key;
+import ortus.boxlang.runtime.types.Argument;
+import ortus.boxlang.runtime.types.BoxLangType;
+
+@BoxBIF
+@BoxMember( type = BoxLangType.NUMERIC )
+public class Fix extends BIF {
+
+	/**
+	 * Constructor
+	 */
+	public Fix() {
+		super();
+		declaredArguments = new Argument[] {
+		    new Argument( true, "numeric", Key.number )
+		};
+	}
+
+	/**
+	 * Converts a real number to an integer
+	 * 
+	 * @param context   The context in which the BIF is being invoked.
+	 * @param arguments Argument scope for the BIF.
+	 * 
+	 * @argument.number The number to convert to an integer
+	 */
+	public Object invoke( IBoxContext context, ArgumentsScope arguments ) {
+		double number = arguments.getAsDouble( Key.number );
+		if ( number == 0 )
+			return number;
+		return number > 0 ? StrictMath.floor( number ) : StrictMath.ceil( number );
+	}
+
+}
---
+++ src/test/java/ortus/boxlang/runtime/bifs/global/math/FixTest.java
@@ -0,0 +1,109 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ortus.boxlang.runtime.bifs.global.math;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import ortus.boxlang.runtime.BoxRuntime;
+import ortus.boxlang.runtime.context.IBoxContext;
+import ortus.boxlang.runtime.context.ScriptingBoxContext;
+import ortus.boxlang.runtime.scopes.IScope;
+import ortus.boxlang.runtime.scopes.Key;
+import ortus.boxlang.runtime.scopes.VariablesScope;
+
+public class FixTest {
+
+	static BoxRuntime	instance;
+	static IBoxContext	context;
+	static IScope		variables;
+	static Key			result	= new Key( "result" );
+
+	@BeforeAll
+	public static void setUp() {
+		instance	= BoxRuntime.getInstance( true );
+		context		= new ScriptingBoxContext( instance.getRuntimeContext() );
+		variables	= context.getScopeNearby( VariablesScope.name );
+	}
+
+	@AfterAll
+	public static void teardown() {
+		instance.shutdown();
+	}
+
+	@BeforeEach
+	public void setupEach() {
+		variables.clear();
+	}
+
+	@DisplayName( "It returns a fixed integer" )
+	@Test
+	public void testFixFunction() {
+		instance.executeSource(
+		    """
+		    result = fix(1.8);
+		    """,
+		    context );
+		assertThat( variables.get( result ) ).isEqualTo( 1 );
+
+		instance.executeSource(
+		    """
+		    result = fix(-2.8);
+		    """,
+		    context );
+		assertThat( variables.get( result ) ).isEqualTo( -2 );
+
+		instance.executeSource(
+		    """
+		    result = fix(0);
+		    """,
+		    context );
+		assertThat( variables.get( result ) ).isEqualTo( 0 );
+	}
+
+	@DisplayName( "It returns a fixed integer member" )
+	@Test
+	public void testFixMember() {
+		instance.executeSource(
+		    """
+		    result = (1.8).fix();
+		    """,
+		    context );
+		assertThat( variables.get( result ) ).isEqualTo( 1 );
+
+		instance.executeSource(
+		    """
+		    result = (-2.8).fix();
+		    """,
+		    context );
+		assertThat( variables.get( result ) ).isEqualTo( -2 );
+
+		instance.executeSource(
+		    """
+		    result = (0).fix();
+		    """,
+		    context );
+		assertThat( variables.get( result ) ).isEqualTo( 0 );
+	}
+
+}
