90e5fffd6f16ef72a20d85e7b6c68f0b1d7e3195
Grant Copley <gcopley@ortussolutions.com>
2024-01-13 10:47:17 -0600

    Initial implementation of incrementValue( number ) and number.incrementValue()

---
+++ src/test/java/ortus/boxlang/runtime/bifs/global/math/IncrementValueTest.java
@@ -0,0 +1,115 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package ortus.boxlang.runtime.bifs.global.math;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import ortus.boxlang.runtime.BoxRuntime;
+import ortus.boxlang.runtime.context.IBoxContext;
+import ortus.boxlang.runtime.context.ScriptingBoxContext;
+import ortus.boxlang.runtime.scopes.IScope;
+import ortus.boxlang.runtime.scopes.Key;
+import ortus.boxlang.runtime.scopes.VariablesScope;
+
+public class IncrementValueTest {
+
+	static BoxRuntime	instance;
+	static IBoxContext	context;
+	static IScope		variables;
+	static Key			result	= new Key( "result" );
+
+	@BeforeAll
+	public static void setUp() {
+		instance	= BoxRuntime.getInstance( true );
+		context		= new ScriptingBoxContext( instance.getRuntimeContext() );
+		variables	= context.getScopeNearby( VariablesScope.name );
+	}
+
+	@AfterAll
+	public static void teardown() {
+		instance.shutdown();
+	}
+
+	@BeforeEach
+	public void setupEach() {
+		variables.clear();
+	}
+
+	@DisplayName( "It increments the integer part" )
+	@Test
+	public void testItIncrementsIntegerPart() {
+		double testNumber;
+
+		testNumber = 1.8;
+		instance.executeSource(
+		    """
+		    result = incrementValue(1.8);
+		    """,
+		    context );
+		assertThat( variables.getAsDouble( result ) ).isEqualTo( ++testNumber );
+
+		testNumber = -2.8;
+		instance.executeSource(
+		    """
+		    result = incrementValue(-2.8);
+		    """,
+		    context );
+		assertThat( variables.getAsDouble( result ) ).isEqualTo( testNumber + 1 );
+
+		testNumber = 0;
+		instance.executeSource(
+		    """
+		    result = incrementValue(0);
+		    """,
+		    context );
+		assertThat( variables.getAsDouble( result ) ).isEqualTo( testNumber + 1 );
+	}
+
+	@DisplayName( "It increments the integer part member function" )
+	@Test
+	public void testItIncrementsIntegerPartMemberFunction() {
+		double testNumber;
+
+		testNumber = 1.8;
+		instance.executeSource(
+		    """
+		    result = (1.8).incrementValue();
+		    """,
+		    context );
+		assertThat( variables.getAsDouble( result ) ).isEqualTo( ++testNumber );
+
+		testNumber = -2.8;
+		instance.executeSource(
+		    """
+		    result = (-2.8).incrementValue();
+		    """,
+		    context );
+		assertThat( variables.getAsDouble( result ) ).isEqualTo( testNumber + 1 );
+
+		testNumber = 0;
+		instance.executeSource(
+		    """
+		    result = (0).incrementValue();
+		    """,
+		    context );
+		assertThat( variables.getAsDouble( result ) ).isEqualTo( testNumber + 1 );
+	}
+}
