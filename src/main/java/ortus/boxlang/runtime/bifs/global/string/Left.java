11096d22e1dddef0dbbb10a24dba3fad197fdf8f
Grant Copley <gcopley@ortussolutions.com>
2024-01-16 17:51:09 -0600

    Initial implementation of left(string, count) and string.left( count )

---
+++ src/main/java/ortus/boxlang/runtime/bifs/global/string/Left.java
@@ -0,0 +1,55 @@
+package ortus.boxlang.runtime.bifs.global.string;
+
+import ortus.boxlang.runtime.bifs.BIF;
+import ortus.boxlang.runtime.bifs.BoxBIF;
+import ortus.boxlang.runtime.bifs.BoxMember;
+import ortus.boxlang.runtime.context.IBoxContext;
+import ortus.boxlang.runtime.scopes.ArgumentsScope;
+import ortus.boxlang.runtime.scopes.Key;
+import ortus.boxlang.runtime.types.Argument;
+import ortus.boxlang.runtime.types.BoxLangType;
+import ortus.boxlang.runtime.types.exceptions.BoxRuntimeException;
+
+@BoxBIF
+@BoxMember( type = BoxLangType.STRING, name = "Left" )
+public class Left extends BIF {
+
+	/**
+	 * Constructor
+	 */
+	public Left() {
+		super();
+		declaredArguments = new Argument[] {
+		    new Argument( true, "string", Key.string ),
+		    new Argument( true, "integer", Key.count )
+		};
+	}
+
+	/**
+	 * Extract the leftmost count characters from a string
+	 *
+	 * @param context   The context in which the BIF is being invoked.
+	 * @param arguments Argument scope for the BIF.
+	 *
+	 * @argument.string The string to extract from
+	 * 
+	 * @argument.count The number of characters to retrieve.
+	 */
+	public Object invoke( IBoxContext context, ArgumentsScope arguments ) {
+		String	input	= arguments.getAsString( Key.string );
+		int		count	= arguments.getAsInteger( Key.count );
+
+		// Check if count is within valid bounds
+		if ( count <= 0 ) {
+			throw new BoxRuntimeException( "Count must be greater than 0" );
+		}
+
+		// Ensure count doesn't exceed the length of the input string
+		if ( count > input.length() ) {
+			count = input.length();
+		}
+
+		// Extract the leftmost substring
+		return input.substring( 0, count );
+	}
+}
---
+++ src/test/java/ortus/boxlang/runtime/bifs/global/string/LeftTest.java
@@ -0,0 +1,96 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package ortus.boxlang.runtime.bifs.global.string;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import ortus.boxlang.runtime.BoxRuntime;
+import ortus.boxlang.runtime.context.IBoxContext;
+import ortus.boxlang.runtime.context.ScriptingBoxContext;
+import ortus.boxlang.runtime.scopes.IScope;
+import ortus.boxlang.runtime.scopes.Key;
+import ortus.boxlang.runtime.scopes.VariablesScope;
+import ortus.boxlang.runtime.types.exceptions.BoxRuntimeException;
+
+public class LeftTest {
+
+	static BoxRuntime	instance;
+	static IBoxContext	context;
+	static IScope		variables;
+	static Key			result	= new Key( "result" );
+
+	@BeforeAll
+	public static void setUp() {
+		instance	= BoxRuntime.getInstance( true );
+		context		= new ScriptingBoxContext( instance.getRuntimeContext() );
+		variables	= context.getScopeNearby( VariablesScope.name );
+	}
+
+	@AfterAll
+	public static void teardown() {
+		instance.shutdown();
+	}
+
+	@BeforeEach
+	public void setupEach() {
+		variables.clear();
+	}
+
+	@DisplayName( "It extracts leftmost characters from a string" )
+	@Test
+	public void testItExtractsLeftmostCharacters() {
+		instance.executeSource(
+		    """
+		    result = left("abcdef", 3);
+		    """,
+		    context );
+		assertThat( variables.get( result ) ).isEqualTo( "abc" );
+	}
+
+	@DisplayName( "It extracts leftmost characters as member" )
+	@Test
+	public void testItExtractsLeftmostCharactersMember() {
+		instance.executeSource(
+		    """
+		    value = "abcdef";
+		    result = value.left(3);
+		    result2 = "xyz".left(2);
+		    """,
+		    context );
+		assertThat( variables.get( result ) ).isEqualTo( "abc" );
+		assertThat( variables.get( Key.of( "result2" ) ) ).isEqualTo( "xy" );
+	}
+
+	@DisplayName( "It throws exception for zero count" )
+	@Test
+	public void testItThrowsExceptionForZeroCount() {
+		assertThrows(
+		    BoxRuntimeException.class,
+		    () -> instance.executeSource(
+		        """
+		        value = "abcdef";
+		              result = value.left(0);
+		              """,
+		        context )
+		);
+	}
+}
