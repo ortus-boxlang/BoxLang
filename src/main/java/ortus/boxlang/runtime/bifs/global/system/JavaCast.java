0f50c5064abe6dbdde12b38b17beeba85966bc65
Luis Majano <lmajano@gmail.com>
2024-01-17 13:31:36 +0100

    Updating Gran'ts great work, but needed cleanup to leverage the Generic Caster

--- src/main/java/ortus/boxlang/runtime/bifs/global/system/JavaCast.java
+++ src/main/java/ortus/boxlang/runtime/bifs/global/system/JavaCast.java
@@ -21,25 +21,9 @@ import ortus.boxlang.runtime.dynamic.casters.GenericCaster;
 import ortus.boxlang.runtime.scopes.ArgumentsScope;
 import ortus.boxlang.runtime.scopes.Key;
 import ortus.boxlang.runtime.types.Argument;
-import ortus.boxlang.runtime.types.exceptions.BoxRuntimeException;
 
 @BoxBIF
 public class JavaCast extends BIF {
-
-	public static final class types {
-
-		public static final Key	BOOLEAN		= Key.of( "boolean" );
-		public static final Key	DOUBLE		= Key.of( "double" );
-		public static final Key	FLOAT		= Key.of( "float" );
-		public static final Key	INT			= Key.of( "int" );
-		public static final Key	LONG		= Key.of( "long" );
-		public static final Key	STRING		= Key.of( "string" );
-		public static final Key	NULL		= Key.of( "null" );
-		public static final Key	BYTE		= Key.of( "byte" );
-		public static final Key	BIGDECIMAL	= Key.of( "bigdecimal" );
-		public static final Key	CHAR		= Key.of( "char" );
-		public static final Key	SHORT		= Key.of( "short" );
-	}
 
 	/**
 	 * Constructor
@@ -59,7 +43,7 @@ public class JavaCast extends BIF {
 	 * @param arguments Argument scope for the BIF.
 	 *
 	 * @argument.type The name of a Java primitive or a Java class name.
-	 * 
+	 *
 	 * @argument.variable The variable, Java object, or array to cast.
 	 */
 	public Object invoke( IBoxContext context, ArgumentsScope arguments ) {
@@ -70,32 +54,6 @@ public class JavaCast extends BIF {
 			return null;
 		}
 
-		Key typeKey = Key.of( type );
-
-		if ( typeKey.equals( types.BOOLEAN ) ) {
-			return GenericCaster.cast( variable, "boolean" );
-		} else if ( typeKey.equals( types.DOUBLE ) ) {
-			return GenericCaster.cast( variable, "double" );
-		} else if ( typeKey.equals( types.FLOAT ) ) {
-			return GenericCaster.cast( variable, "float" );
-		} else if ( typeKey.equals( types.INT ) ) {
-			return GenericCaster.cast( variable, "int" );
-		} else if ( typeKey.equals( types.LONG ) ) {
-			return GenericCaster.cast( variable, "long" );
-		} else if ( typeKey.equals( types.STRING ) ) {
-			return GenericCaster.cast( variable, "string" );
-		} else if ( typeKey.equals( types.NULL ) ) {
-			return GenericCaster.cast( variable, "null" );
-		} else if ( typeKey.equals( types.BYTE ) ) {
-			return GenericCaster.cast( variable, "byte" );
-		} else if ( typeKey.equals( types.BIGDECIMAL ) ) {
-			return GenericCaster.cast( variable, "bigdecimal" );
-		} else if ( typeKey.equals( types.CHAR ) ) {
-			return GenericCaster.cast( variable, "char" );
-		} else if ( typeKey.equals( types.SHORT ) ) {
-			return GenericCaster.cast( variable, "short" );
-		} else {
-			throw new BoxRuntimeException( "Unsupported Java cast type: " + type );
-		}
+		return GenericCaster.cast( variable, Key.of( type ) );
 	}
 }
