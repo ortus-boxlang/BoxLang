3c8797140a563d6f3fbbcfd0d2bb8a3e69a75efa
Grant Copley <gcopley@ortussolutions.com>
2024-01-17 01:10:08 -0600

    Initial implementation of reverse(string) and string.reverse()

---
+++ src/main/java/ortus/boxlang/runtime/bifs/global/string/Reverse.java
@@ -0,0 +1,58 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package ortus.boxlang.runtime.bifs.global.string;
+
+import ortus.boxlang.runtime.bifs.BIF;
+import ortus.boxlang.runtime.bifs.BoxBIF;
+import ortus.boxlang.runtime.bifs.BoxMember;
+import ortus.boxlang.runtime.context.IBoxContext;
+import ortus.boxlang.runtime.scopes.ArgumentsScope;
+import ortus.boxlang.runtime.scopes.Key;
+import ortus.boxlang.runtime.types.Argument;
+import ortus.boxlang.runtime.types.BoxLangType;
+
+@BoxBIF
+@BoxMember( type = BoxLangType.STRING, name = "Reverse" )
+public class Reverse extends BIF {
+
+	/**
+	 * Constructor
+	 */
+	public Reverse() {
+		super();
+		declaredArguments = new Argument[] {
+		    new Argument( true, "string", Key.string )
+		};
+	}
+
+	/**
+	 * Reverse the order of characters in a string
+	 *
+	 * @param context   The context in which the BIF is being invoked.
+	 * @param arguments Argument scope for the BIF.
+	 *
+	 * @argument.string The string to reverse.
+	 */
+	public Object invoke( IBoxContext context, ArgumentsScope arguments ) {
+		String			input		= arguments.getAsString( Key.string );
+
+		// Create a StringBuilder to reverse the string
+		StringBuilder	reversed	= new StringBuilder( input );
+		reversed.reverse();
+
+		// Convert the StringBuilder back to a String
+		return reversed.toString();
+	}
+}
