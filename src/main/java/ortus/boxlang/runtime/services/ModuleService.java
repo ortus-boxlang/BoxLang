5e0509363a2e3b31859c398e30ecd621e4e06007
Luis Majano <lmajano@gmail.com>
2024-01-17 23:24:45 +0100

    poc module service start [no ci] wip

---
+++ src/main/java/ortus/boxlang/runtime/services/ModuleService.java
@@ -0,0 +1,88 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ortus.boxlang.runtime.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ortus.boxlang.runtime.BoxRuntime;
+
+/**
+ * This service is in charge of managing BoxLang modules
+ */
+public class ModuleService extends BaseService {
+
+	/**
+	 * --------------------------------------------------------------------------
+	 * Private Properties
+	 * --------------------------------------------------------------------------
+	 */
+
+	private static final String	CORE_MODULES	= "ortus.boxlang.runtime.modules";
+
+	/**
+	 * Logger
+	 */
+	private static final Logger	logger			= LoggerFactory.getLogger( ModuleService.class );
+
+	/**
+	 * --------------------------------------------------------------------------
+	 * Constructor(s)
+	 * --------------------------------------------------------------------------
+	 */
+
+	/**
+	 * Constructor
+	 *
+	 * @param runtime The runtime instance
+	 */
+	public ModuleService( BoxRuntime runtime ) {
+		super( runtime );
+	}
+
+	/**
+	 * --------------------------------------------------------------------------
+	 * Runtime Service Event Methods
+	 * --------------------------------------------------------------------------
+	 */
+
+	/**
+	 * The startup event is fired when the runtime starts up
+	 */
+	@Override
+	public void onStartup() {
+		logger.info( "ModuleService.onStartup()" );
+	}
+
+	/**
+	 * The configuration load event is fired when the runtime loads its configuration
+	 */
+	@Override
+	public void onConfigurationLoad() {
+		logger.info( "ModuleService.onConfigurationLoad()" );
+	}
+
+	/**
+	 * The shutdown event is fired when the runtime shuts down
+	 */
+	@Override
+	public void onShutdown() {
+		logger.info( "ModuleService.onShutdown()" );
+	}
+
+}
