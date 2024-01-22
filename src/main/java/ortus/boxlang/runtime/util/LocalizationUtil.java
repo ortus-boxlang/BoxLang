4330076582138d8f54d26555d46a4658945bbd0f
jclausen <jclausen@ortussolutions.com>
2024-01-22 11:59:22 -0500

    add commonAliases to match Lucee/ACF alias implementations

--- src/main/java/ortus/boxlang/runtime/util/LocalizationUtil.java
+++ src/main/java/ortus/boxlang/runtime/util/LocalizationUtil.java
@@ -28,14 +28,14 @@ import ortus.boxlang.runtime.context.IBoxContext;
 import ortus.boxlang.runtime.dynamic.casters.StringCaster;
 import ortus.boxlang.runtime.scopes.Key;
 import ortus.boxlang.runtime.types.DateTime;
-import ortus.boxlang.runtime.types.Struct;
+import ortus.boxlang.runtime.types.immutable.ImmutableStruct;
 
 /**
  * A Collection of Common Static Properties and Methods to support Localization
  **/
 public final class LocalizationUtil {
 
-	public static final Struct commonLocales = new Struct(
+	public static final ImmutableStruct	commonLocales	= new ImmutableStruct(
 	    new HashMap<Key, Locale>() {
 
 		    {
@@ -70,10 +70,84 @@ public final class LocalizationUtil {
 	    }
 	);
 
+	public static final ImmutableStruct	localeAliases	= new ImmutableStruct(
+	    new HashMap<Key, Locale>() {
+
+		    {
+			    put( Key.of( "Albanian (Albania)" ), new Locale( "sq", "AL" ) );
+			    put( Key.of( "Arabic (Algeria)" ), new Locale( "ar", "DZ" ) );
+			    put( Key.of( "Arabic (Bahrain)" ), new Locale( "ar", "BH" ) );
+			    put( Key.of( "Arabic (Egypt)" ), new Locale( "ar", "EG" ) );
+			    put( Key.of( "Arabic (Iraq)" ), new Locale( "ar", "IQ" ) );
+			    put( Key.of( "Arabic (Jordan)" ), new Locale( "ar", "JO" ) );
+			    put( Key.of( "Arabic (Kuwait)" ), new Locale( "ar", "KW" ) );
+			    put( Key.of( "Arabic (Lebanon)" ), new Locale( "ar", "LB" ) );
+			    put( Key.of( "Arabic (Libya)" ), new Locale( "ar", "LY" ) );
+			    put( Key.of( "Arabic (Morocco)" ), new Locale( "ar", "MA" ) );
+			    put( Key.of( "Arabic (Oman)" ), new Locale( "ar", "OM" ) );
+			    put( Key.of( "Arabic (Qatar)" ), new Locale( "ar", "QA" ) );
+			    put( Key.of( "Arabic (Saudi Arabia)" ), new Locale( "ar", "SA" ) );
+			    put( Key.of( "Arabic (Sudan)" ), new Locale( "ar", "SD" ) );
+			    put( Key.of( "Arabic (Syria)" ), new Locale( "ar", "SY" ) );
+			    put( Key.of( "Arabic (Tunisia)" ), new Locale( "ar", "TN" ) );
+			    put( Key.of( "Arabic (United Arab Emirates)" ), new Locale( "ar", "AE" ) );
+			    put( Key.of( "Arabic (Yemen)" ), new Locale( "ar", "YE" ) );
+			    put( Key.of( "Chinese (China)" ), Locale.CHINA );
+			    put( Key.of( "Chinese (Hong Kong)" ), new Locale( "zh", "HK" ) );
+			    put( Key.of( "Chinese (Singapore)" ), new Locale( "zh", "SG" ) );
+			    put( Key.of( "Chinese (Taiwan)" ), new Locale( "zh", "TW" ) );
+			    put( Key.of( "Dutch (Belgian)" ), new Locale( "nl", "BE" ) );
+			    put( Key.of( "Dutch (Belgium)" ), ( Locale ) get( Key.of( "dutch (belgian)" ) ) );
+			    put( Key.of( "Dutch (Standard)" ), new Locale( "nl", "NL" ) );
+			    put( Key.of( "English (Australian)" ), new Locale( "en", "AU" ) );
+			    put( Key.of( "English (Australia)" ), ( Locale ) get( Key.of( "english (australian)" ) ) );
+			    put( Key.of( "English (Canadian)" ), Locale.CANADA );
+			    put( Key.of( "English (Canada)" ), Locale.CANADA );
+			    put( Key.of( "English (New zealand)" ), new Locale( "en", "NZ" ) );
+			    put( Key.of( "English (UK)" ), Locale.UK );
+			    put( Key.of( "English (United Kingdom)" ), Locale.UK );
+			    put( Key.of( "English (GB)" ), Locale.UK );
+			    put( Key.of( "English (Great Britan)" ), Locale.UK );
+			    put( Key.of( "English (US)" ), Locale.US );
+			    put( Key.of( "English (United States)" ), Locale.US );
+			    put( Key.of( "English (United States of America)" ), Locale.US );
+			    put( Key.of( "English (USA)" ), Locale.US );
+			    put( Key.of( "French (Belgium)" ), new Locale( "fr", "BE" ) );
+			    put( Key.of( "French (Belgian)" ), new Locale( "fr", "BE" ) );
+			    put( Key.of( "French (Canadian)" ), Locale.CANADA_FRENCH );
+			    put( Key.of( "French (Canadia)" ), Locale.CANADA_FRENCH );
+			    put( Key.of( "French (Standard)" ), Locale.FRANCE );
+			    put( Key.of( "French (Swiss)" ), new Locale( "fr", "CH" ) );
+			    put( Key.of( "German (Austrian)" ), new Locale( "de", "AT" ) );
+			    put( Key.of( "German (Austria)" ), new Locale( "de", "AT" ) );
+			    put( Key.of( "German (Standard)" ), Locale.GERMANY );
+			    put( Key.of( "German (Swiss)" ), new Locale( "de", "CH" ) );
+			    put( Key.of( "Italian (Standard)" ), Locale.ITALIAN );
+			    put( Key.of( "Italian (Swiss)" ), new Locale( "it", "CH" ) );
+			    put( Key.of( "Japanese" ), Locale.JAPANESE );
+			    put( Key.of( "Korean" ), Locale.KOREAN );
+			    put( Key.of( "Norwegian (Bokmal)" ), new Locale( "no", "NO" ) );
+			    put( Key.of( "Norwegian (Nynorsk)" ), new Locale( "no", "NO" ) );
+			    put( Key.of( "Portuguese (Brazilian)" ), new Locale( "pt", "BR" ) );
+			    put( Key.of( "Portuguese (Brazil)" ), ( Locale ) get( Key.of( "portuguese (brazilian)" ) ) );
+			    put( Key.of( "Portuguese (Standard)" ), new Locale( "pt", "PT" ) );
+			    put( Key.of( "Rhaeto-Romance (Swiss)" ), new Locale( "rm", "CH" ) );
+			    put( Key.of( "Rhaeto-Romance (Swiss)" ), new Locale( "rm", "CH" ) );
+			    put( Key.of( "Spanish (Modern)" ), new Locale( "es", "ES" ) );
+			    put( Key.of( "Spanish (Standard)" ), new Locale( "es", "ES" ) );
+			    put( Key.of( "Swedish" ), new Locale( "sv", "SE" ) );
+			    put( Key.of( "Welsh" ), new Locale( "cy", "GB" ) );
+		    }
+	    }
+
+	);
+
 	public static Locale parseLocale( String requestedLocale ) {
 		Locale localeObj = null;
 		if ( requestedLocale != null && commonLocales.containsKey( requestedLocale ) ) {
 			localeObj = ( Locale ) commonLocales.get( requestedLocale );
+		} else if ( requestedLocale != null && localeAliases.containsKey( requestedLocale ) ) {
+			localeObj = ( Locale ) localeAliases.get( requestedLocale );
 		} else if ( requestedLocale != null ) {
 			var		localeParts	= requestedLocale.split( "-|_| " );
 			String	ISOLang		= localeParts[ 0 ];
