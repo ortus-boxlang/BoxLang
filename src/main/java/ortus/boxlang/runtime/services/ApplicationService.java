583816027e7a0b7047bc1934db602f9bc5dd8fc4
Luis Majano <lmajano@gmail.com>
2024-01-22 15:35:14 +0100

    - Docs - New convenience methods to get all app names, and to check existence - Parallel streams for shutdown

--- src/main/java/ortus/boxlang/runtime/services/ApplicationService.java
+++ src/main/java/ortus/boxlang/runtime/services/ApplicationService.java
@@ -20,6 +20,9 @@ package ortus.boxlang.runtime.services;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import ortus.boxlang.runtime.BoxRuntime;
 import ortus.boxlang.runtime.application.Application;
 import ortus.boxlang.runtime.scopes.Key;
@@ -30,17 +33,42 @@ import ortus.boxlang.runtime.scopes.Key;
 public class ApplicationService extends BaseService {
 
 	/**
+	 * --------------------------------------------------------------------------
+	 * Private Properties
+	 * --------------------------------------------------------------------------
+	 */
+
+	/**
 	 * The applications for this runtime
 	 * TODO: timeout applications
 	 */
-	private Map<Key, Application> applications = new ConcurrentHashMap<Key, Application>();
+	private Map<Key, Application>	applications	= new ConcurrentHashMap<>();
+
+	/**
+	 * Logger
+	 */
+	private static final Logger		logger			= LoggerFactory.getLogger( ApplicationService.class );
+
+	/**
+	 * --------------------------------------------------------------------------
+	 * Constructor(s)
+	 * --------------------------------------------------------------------------
+	 */
 
 	/**
 	 * Constructor
+	 *
+	 * @param runtime The runtime instance
 	 */
 	public ApplicationService( BoxRuntime runtime ) {
 		super( runtime );
 	}
+
+	/**
+	 * --------------------------------------------------------------------------
+	 * Application Helper Methods
+	 * --------------------------------------------------------------------------
+	 */
 
 	/**
 	 * Get an application by name, creating if neccessary
@@ -65,15 +93,51 @@ public class ApplicationService extends BaseService {
 		return thisApplication;
 	}
 
-	@Override
-	public void onStartup() {
+	/**
+	 * Check if an application exists
+	 *
+	 * @param name The name of the application
+	 *
+	 * @return True if the application exists
+	 */
+	boolean hasApplication( Key name ) {
+		return applications.containsKey( name );
 	}
 
+	/**
+	 * Get the names of all registered applications
+	 *
+	 * @return The names of all applications
+	 */
+	String[] getApplicationNames() {
+		return applications.keySet()
+		    .stream()
+		    .sorted()
+		    .map( Key::getName )
+		    .toArray( String[]::new );
+	}
+
+	/**
+	 * --------------------------------------------------------------------------
+	 * Runtime Service Event Methods
+	 * --------------------------------------------------------------------------
+	 */
+
+	/**
+	 * The startup event is fired when the runtime starts up
+	 */
+	@Override
+	public void onStartup() {
+		logger.info( "ApplicationService.onStartup()" );
+	}
+
+	/**
+	 * The shutdown event is fired when the runtime shuts down
+	 */
 	@Override
 	public void onShutdown() {
 		// loop over applications and shutdown
-		for ( Application application : applications.values() ) {
-			application.shutdown();
-		}
+		applications.values().parallelStream().forEach( Application::shutdown );
+		logger.info( "ApplicationService.onShutdown()" );
 	}
 }
