b14c02cea3cc0a88e7de5c687b87ffbb7eb6ea38
Grant Copley <gcopley@ortussolutions.com>
2024-01-23 07:47:27 -0600

    Initial implementation of find( substring, string, start ) and string.find( substring, start )

---
+++ src/main/java/ortus/boxlang/runtime/bifs/global/string/Find.java
@@ -0,0 +1,71 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package ortus.boxlang.runtime.bifs.global.string;
+
+import ortus.boxlang.runtime.bifs.BIF;
+import ortus.boxlang.runtime.bifs.BoxBIF;
+import ortus.boxlang.runtime.bifs.BoxMember;
+import ortus.boxlang.runtime.context.IBoxContext;
+import ortus.boxlang.runtime.scopes.ArgumentsScope;
+import ortus.boxlang.runtime.scopes.Key;
+import ortus.boxlang.runtime.types.Argument;
+import ortus.boxlang.runtime.types.BoxLangType;
+
+@BoxBIF
+@BoxMember( type = BoxLangType.STRING, name = "Find", objectArgument = "string" )
+public class Find extends BIF {
+
+	/**
+	 * Constructor
+	 */
+	public Find() {
+		super();
+		declaredArguments = new Argument[] {
+		    new Argument( true, "string", Key.substring ),
+		    new Argument( true, "string", Key.string ),
+		    new Argument( false, "integer", Key.start, 1 )
+		};
+	}
+
+	/**
+	 * Finds the first occurrence of a substring in a string, from a specified start position.
+	 *
+	 * @param context   The context in which the BIF is being invoked.
+	 * @param arguments Argument scope for the BIF.
+	 *
+	 * @argument.substring The string you are looking for.
+	 * 
+	 * @argument.string The string to search in.
+	 * 
+	 * @argument.start The position from which to start searching in the string. Default is 1.
+	 *
+	 * @return Returns the position of the first occurrence of the substring. If the substring is not found, returns zero.
+	 */
+	public Object invoke( IBoxContext context, ArgumentsScope arguments ) {
+		String	substring	= arguments.getAsString( Key.substring );
+		String	input		= arguments.getAsString( Key.string );
+		int		start		= arguments.getAsInteger( Key.start );
+
+		// Check if the start position is within valid bounds
+		if ( start < 1 ) {
+			start = 1; // Adjust start to 1 if it's less than 1
+		}
+
+		// Find the first occurrence of the substring from the specified start position
+		int position = input.indexOf( substring, start - 1 ) + 1;
+
+		return position > 0 ? position : 0; // Return position or 0 if not found
+	}
+}
---
+++ src/test/java/ortus/boxlang/runtime/bifs/global/string/FindTest.java
@@ -0,0 +1,110 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package ortus.boxlang.runtime.bifs.global.string;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import ortus.boxlang.runtime.BoxRuntime;
+import ortus.boxlang.runtime.context.IBoxContext;
+import ortus.boxlang.runtime.context.ScriptingRequestBoxContext;
+import ortus.boxlang.runtime.scopes.IScope;
+import ortus.boxlang.runtime.scopes.Key;
+import ortus.boxlang.runtime.scopes.VariablesScope;
+
+public class FindTest {
+
+	static BoxRuntime	instance;
+	static IBoxContext	context;
+	static IScope		variables;
+	static Key			result	= new Key( "result" );
+
+	@BeforeAll
+	public static void setUp() {
+		instance	= BoxRuntime.getInstance( true );
+		context		= new ScriptingRequestBoxContext( instance.getRuntimeContext() );
+		variables	= context.getScopeNearby( VariablesScope.name );
+	}
+
+	@AfterAll
+	public static void teardown() {
+		instance.shutdown();
+	}
+
+	@BeforeEach
+	public void setupEach() {
+		variables.clear();
+	}
+
+	@DisplayName( "It finds the first occurrence of a substring in a string" )
+	@Test
+	public void testFindSubstring() {
+		instance.executeSource(
+		    """
+		    result = find("Lang", "BoxLang is great");
+		    """,
+		    context );
+		assertThat( variables.get( result ) ).isEqualTo( 4 ); // "Lang" starts at position 4
+	}
+
+	@DisplayName( "It returns zero for substring not found" )
+	@Test
+	public void testFindSubstringNotFound() {
+		instance.executeSource(
+		    """
+		    result = find("ColdFusion", "BoxLang is great");
+		    """,
+		    context );
+		assertThat( variables.get( result ) ).isEqualTo( 0 ); // "ColdFusion" is not found
+	}
+
+	@DisplayName( "It finds the first occurrence from a specified start position" )
+	@Test
+	public void testFindSubstringWithStart() {
+		instance.executeSource(
+		    """
+		    result = find("Lang", "BoxLang is great", 2 );
+		    """,
+		    context );
+		assertThat( variables.get( result ) ).isEqualTo( 4 ); // "Lang" starts at position 10 after the 6th position
+	}
+
+	@DisplayName( "It returns for negative start position" )
+	@Test
+	public void testFindNegativeStart() {
+		instance.executeSource(
+		    """
+		    result = find("Lang", "BoxLang is great", -1);
+		    """,
+		    context );
+		assertThat( variables.get( result ) ).isEqualTo( 4 ); // Negative start position should be treated as 1
+	}
+
+	@DisplayName( "It returns for member function call" )
+	@Test
+	public void testFindMemberFunctionCall() {
+		instance.executeSource(
+		    """
+		    result = "BoxLang is great".find("Lang");
+		    """,
+		    context );
+		assertThat( variables.get( result ) ).isEqualTo( 4 ); // "Lang" starts at position 4
+	}
+}
