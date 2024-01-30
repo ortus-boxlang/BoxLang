1921535897a65fa80e9660b5493b2a14b5ca68a2
Jacob Beers <jacobtbeers@gmail.com>
2024-01-29 21:05:03 -0600

    Add license headers

--- src/main/java/ortus/boxlang/debugger/DebugAdapter.java
+++ src/main/java/ortus/boxlang/debugger/DebugAdapter.java
@@ -1,3 +1,20 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ortus.boxlang.debugger;
 
 import java.io.BufferedReader;
@@ -89,6 +106,7 @@ public class DebugAdapter {
 							}
 						}
 					} catch ( IOException e ) {
+						// TODO handle this exception
 						e.printStackTrace();
 						break;
 					}
@@ -101,7 +119,7 @@ public class DebugAdapter {
 	}
 
 	private IDebugRequest parseDebugRequest( String json ) {
-		Map<String, Object>	requestData	= ( Map ) JsonUtil.fromJson( json );
+		Map<String, Object>	requestData	= ( Map<String, Object> ) JsonUtil.fromJson( json );
 		String				command		= ( String ) requestData.get( "command" );
 
 		this.logger.info( "Received command {}", command );
