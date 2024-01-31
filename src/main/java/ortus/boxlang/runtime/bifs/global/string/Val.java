e6adda051cf0568acb2fe19cec5dc878e852062c
Brad Wood <brad@bradwood.com>
2024-01-31 16:31:09 -0600

    Add val()

---
+++ src/main/java/ortus/boxlang/runtime/bifs/global/string/Val.java
@@ -0,0 +1,81 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package ortus.boxlang.runtime.bifs.global.string;
+
+import ortus.boxlang.runtime.bifs.BIF;
+import ortus.boxlang.runtime.bifs.BoxBIF;
+import ortus.boxlang.runtime.bifs.BoxMember;
+import ortus.boxlang.runtime.context.IBoxContext;
+import ortus.boxlang.runtime.scopes.ArgumentsScope;
+import ortus.boxlang.runtime.scopes.Key;
+import ortus.boxlang.runtime.types.Argument;
+import ortus.boxlang.runtime.types.BoxLangType;
+
+@BoxBIF
+@BoxMember( type = BoxLangType.STRING, name = "Val" )
+public class Val extends BIF {
+
+	/**
+	 * Constructor
+	 */
+	public Val() {
+		super();
+		declaredArguments = new Argument[] {
+		    new Argument( true, "string", Key.string )
+		};
+	}
+
+	/**
+	 * Converts numeric characters that occur at the beginning of a string to a number. If conversion fails, returns zero.
+	 *
+	 * @param context   The context in which the BIF is being invoked.
+	 * @param arguments Argument scope for the BIF.
+	 *
+	 * @argument.string The string to parse
+	 */
+	public Object invoke( IBoxContext context, ArgumentsScope arguments ) {
+		var		input		= arguments.getAsString( Key.string );
+		var		result		= new StringBuilder();
+		boolean	foundDot	= false;
+		// Loop over each character in the string
+		for ( var c : input.getBytes() ) {
+			// If the character is not a digit
+			if ( ( c < 48 || c > 57 ) && c != 46 ) {
+				// we're done
+				break;
+			}
+			// The FIRST period is allowed
+			if ( c == 46 ) {
+				// But subsequent periods are not
+				if ( foundDot ) {
+					break;
+				}
+				foundDot = true;
+			}
+			// Build up the result
+			result.append( ( char ) c );
+		}
+		// If no digits were found, return 0
+		if ( result.length() == 0 ) {
+			return 0;
+		}
+		// A single period is not a valid number
+		if ( result.toString().equals( "." ) ) {
+			return 0;
+		}
+		// Return the result as a double
+		return Double.parseDouble( result.toString() );
+	}
+}
---
+++ src/test/java/ortus/boxlang/runtime/bifs/global/string/ValTest.java
@@ -0,0 +1,110 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package ortus.boxlang.runtime.bifs.global.string;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import ortus.boxlang.runtime.BoxRuntime;
+import ortus.boxlang.runtime.context.IBoxContext;
+import ortus.boxlang.runtime.context.ScriptingRequestBoxContext;
+import ortus.boxlang.runtime.scopes.IScope;
+import ortus.boxlang.runtime.scopes.Key;
+import ortus.boxlang.runtime.scopes.VariablesScope;
+
+public class ValTest {
+
+	static BoxRuntime	instance;
+	static IBoxContext	context;
+	static IScope		variables;
+	static Key			result	= new Key( "result" );
+
+	@BeforeAll
+	public static void setUp() {
+		instance	= BoxRuntime.getInstance( true );
+		context		= new ScriptingRequestBoxContext( instance.getRuntimeContext() );
+		variables	= context.getScopeNearby( VariablesScope.name );
+	}
+
+	@AfterAll
+	public static void teardown() {
+		instance.shutdown();
+	}
+
+	@BeforeEach
+	public void setupEach() {
+		variables.clear();
+	}
+
+	@DisplayName( "It extracts number from string" )
+	@Test
+	public void testItExtractsNumber() {
+		instance.executeSource(
+		    """
+		    result = val("abcdef");
+		    result2 = val("abcdef");
+		    result3 = val("abc123");
+		    result4 = val("123abcdef");
+		    result5 = val("123");
+		    result6 = val(".123");
+		    result7 = val("123.456");
+		    result8 = val(".");
+		    result9 = val(".sdf");
+		      """,
+		    context );
+		assertThat( variables.get( result ) ).isEqualTo( 0 );
+		assertThat( variables.get( Key.of( "result2" ) ) ).isEqualTo( 0 );
+		assertThat( variables.get( Key.of( "result3" ) ) ).isEqualTo( 0 );
+		assertThat( variables.get( Key.of( "result4" ) ) ).isEqualTo( 123 );
+		assertThat( variables.get( Key.of( "result5" ) ) ).isEqualTo( 123 );
+		assertThat( variables.get( Key.of( "result6" ) ) ).isEqualTo( .123 );
+		assertThat( variables.get( Key.of( "result7" ) ) ).isEqualTo( 123.456 );
+		assertThat( variables.get( Key.of( "result8" ) ) ).isEqualTo( 0 );
+		assertThat( variables.get( Key.of( "result9" ) ) ).isEqualTo( 0 );
+	}
+
+	@DisplayName( "It extracts number from string Member" )
+	@Test
+	public void testItExtractsNumberMember() {
+		instance.executeSource(
+		    """
+		    result = "abcdef".val();
+		    result2 = "abcdef".val();
+		    result3 = "abc123".val();
+		    result4 = "123abcdef".val();
+		    result5 = "123".val();
+		    result6 = ".123".val();
+		    result7 = "123.456".val();
+		    result8 = ".".val();
+		    result9 = ".sdf".val();
+		      """,
+		    context );
+		assertThat( variables.get( result ) ).isEqualTo( 0 );
+		assertThat( variables.get( Key.of( "result2" ) ) ).isEqualTo( 0 );
+		assertThat( variables.get( Key.of( "result3" ) ) ).isEqualTo( 0 );
+		assertThat( variables.get( Key.of( "result4" ) ) ).isEqualTo( 123 );
+		assertThat( variables.get( Key.of( "result5" ) ) ).isEqualTo( 123 );
+		assertThat( variables.get( Key.of( "result6" ) ) ).isEqualTo( .123 );
+		assertThat( variables.get( Key.of( "result7" ) ) ).isEqualTo( 123.456 );
+		assertThat( variables.get( Key.of( "result8" ) ) ).isEqualTo( 0 );
+		assertThat( variables.get( Key.of( "result9" ) ) ).isEqualTo( 0 );
+	}
+
+}
