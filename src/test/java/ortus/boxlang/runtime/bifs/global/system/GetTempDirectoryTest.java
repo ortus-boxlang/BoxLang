25ced659357ce0e2bfb5394d5317121f0916c743
Luis Majano <lmajano@gmail.com>
2024-02-19 17:49:44 +0100

    getTempDirectory() BIF completed

---
+++ src/test/java/ortus/boxlang/runtime/bifs/global/system/GetTempDirectoryTest.java
@@ -0,0 +1,52 @@
+package ortus.boxlang.runtime.bifs.global.system;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import ortus.boxlang.runtime.BoxRuntime;
+import ortus.boxlang.runtime.context.IBoxContext;
+import ortus.boxlang.runtime.context.ScriptingRequestBoxContext;
+import ortus.boxlang.runtime.scopes.IScope;
+import ortus.boxlang.runtime.scopes.Key;
+import ortus.boxlang.runtime.scopes.VariablesScope;
+
+public class GetTempDirectoryTest {
+
+	static BoxRuntime	instance;
+	static IBoxContext	context;
+	static IScope		variables;
+	static Key			result	= new Key( "result" );
+
+	@BeforeAll
+	public static void setUp() {
+		instance	= BoxRuntime.getInstance( true );
+		context		= new ScriptingRequestBoxContext( instance.getRuntimeContext() );
+		variables	= context.getScopeNearby( VariablesScope.name );
+	}
+
+	@AfterAll
+	public static void teardown() {
+		instance.shutdown();
+	}
+
+	@BeforeEach
+	public void setupEach() {
+		variables.clear();
+	}
+
+	@DisplayName( "It can get the system temp directory" )
+	@Test
+	public void testGetTempDirectory() {
+		instance.executeSource(
+		    """
+		    result = getTempDirectory()
+		    """,
+		    context );
+		assertThat( variables.get( result ) ).isEqualTo( System.getProperty( "java.io.tmpdir" ) );
+	}
+}
