13c90b27dfd60e38dff7ee63a02e62e6cf77b59c
jclausen <jclausen@ortussolutions.com>
2024-02-22 10:46:00 -0500

    implements the BIF StringMap

---
+++ src/test/java/ortus/boxlang/runtime/bifs/global/string/StringMapTest.java
@@ -0,0 +1,86 @@
+
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ortus.boxlang.runtime.bifs.global.string;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import ortus.boxlang.runtime.BoxRuntime;
+import ortus.boxlang.runtime.context.IBoxContext;
+import ortus.boxlang.runtime.context.ScriptingRequestBoxContext;
+import ortus.boxlang.runtime.scopes.IScope;
+import ortus.boxlang.runtime.scopes.Key;
+import ortus.boxlang.runtime.scopes.VariablesScope;
+import ortus.boxlang.runtime.types.Array;
+import ortus.boxlang.runtime.types.ListUtil;
+
+public class StringMapTest {
+
+	static BoxRuntime	instance;
+	IBoxContext			context;
+	IScope				variables;
+	static Key			result	= new Key( "result" );
+
+	@BeforeAll
+	public static void setUp() {
+		instance = BoxRuntime.getInstance( true );
+	}
+
+	@AfterAll
+	public static void teardown() {
+
+	}
+
+	@BeforeEach
+	public void setupEach() {
+		context		= new ScriptingRequestBoxContext( instance.getRuntimeContext() );
+		variables	= context.getScopeNearby( VariablesScope.name );
+	}
+
+	@DisplayName( "Tests the BIF StringMap" )
+	@Test
+	public void testBIF() {
+		instance.executeSource(
+		    """
+		        nums = "12345";
+
+		        function mapFn( value, i ){
+		            return value * 2;
+		        };
+
+		        result = StringMap( nums, mapFn );
+		    """,
+		    context );
+		Array result = ListUtil.asList( variables.getAsString( Key.of( "result" ) ), "" );
+		assertThat( result.size() ).isEqualTo( 6 );
+		assertThat( result.get( 0 ) ).isEqualTo( "2" );
+		assertThat( result.get( 1 ) ).isEqualTo( "4" );
+		assertThat( result.get( 2 ) ).isEqualTo( "6" );
+		assertThat( result.get( 3 ) ).isEqualTo( "8" );
+		assertThat( result.get( 4 ) ).isEqualTo( "1" );
+		assertThat( result.get( 5 ) ).isEqualTo( "0" );
+	}
+
+}
