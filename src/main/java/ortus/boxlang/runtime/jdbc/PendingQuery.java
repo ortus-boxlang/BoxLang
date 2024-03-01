3d37314048fd4afd77cce511623c2f2454f31e11
Eric Peterson <eric@elpete.com>
2024-03-01 10:35:05 -0700

    Javadocs for PendingQuery

--- src/main/java/ortus/boxlang/runtime/jdbc/PendingQuery.java
+++ src/main/java/ortus/boxlang/runtime/jdbc/PendingQuery.java
@@ -24,33 +24,92 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
+import javax.annotation.Nonnull;
+
 import ortus.boxlang.runtime.types.Array;
 import ortus.boxlang.runtime.types.IStruct;
 import ortus.boxlang.runtime.types.exceptions.BoxRuntimeException;
 import ortus.boxlang.runtime.types.exceptions.DatabaseException;
 import ortus.boxlang.runtime.types.util.ListUtil;
 
+/**
+ * This class represents a query and any parameters/bindings before being executed.
+ * After calling {@link #execute(Connection)}, it returns an {@link ExecutedQuery} with a reference to this object.
+ */
 public class PendingQuery {
 
-	public String				sql;
-	public String				originalSql;
-	public List<QueryParameter>	parameters;
+	/**
+	 * The SQL string to execute.
+	 * If this SQL has parameters, they should be represented either as question marks (`?`)
+	 * or as named bindings, prefixed with a colon (`:`)
+	 */
+	private final String				sql;
 
-	public PendingQuery( String sql, List<QueryParameter> parameters, String originalSql ) {
+	/**
+	 * The original SQL provided to the constructor. When constructing a `PendingQuery` with an
+	 * {@link IStruct} instance, the named parameters — prefixed with a colon (`:`) in the SQL string —
+	 * are replaced with question marks to work with the JDBC query. The SQL string with question marks
+	 * is set as `sql` and the original SQL provided with named parameters is set as `originalSql`.
+	 */
+	private final String				originalSql;
+
+	/**
+	 * A List of QueryParameter instances. The store the value and SQL type of the parameters, in order.
+	 * @see QueryParameter
+	 */
+	private final List<QueryParameter>	parameters;
+
+	/**
+	 * Creates a new PendingQuery instance from a SQL string, a list of parameters, and the original SQL string.
+	 *
+	 * @param sql         The SQL string to execute
+	 * @param parameters  A list of {@link QueryParameter} to use as bindings.
+	 * @param originalSql The original sql string. This will include named parameters if the `PendingQuery` was constructed using an {@link IStruct}.
+	 */
+	public PendingQuery( @Nonnull String sql, @Nonnull List<QueryParameter> parameters, @Nonnull String originalSql ) {
 		this.sql			= sql;
 		this.originalSql	= originalSql;
 		this.parameters		= parameters;
 	}
 
-	public PendingQuery( String sql, List<QueryParameter> parameters ) {
+	/**
+	 * Creates a new PendingQuery instance from a SQL string and a list of parameters.
+	 * This constructor uses the provided SQL string as the original SQL.
+	 *
+	 * @param sql         The SQL string to execute
+	 * @param parameters  A list of {@link QueryParameter} to use as bindings.
+	 */
+	public PendingQuery( @Nonnull String sql, @Nonnull List<QueryParameter> parameters ) {
 		this( sql, parameters, sql );
 	}
 
-	public PendingQuery( String sql, Array parameters ) {
+	/**
+	 * Creates a new PendingQuery instance from a SQL string, a list of parameters, and the original SQL string.
+	 *
+	 * @param sql         The SQL string to execute
+	 * @param parameters  An {@link Array} of `queryparam` {@link IStruct} instances to convert to {@link QueryParameter} instances and use as bindings.
+	 */
+	public PendingQuery( @Nonnull String sql, @Nonnull Array parameters ) {
 		this( sql, parameters.stream().map( QueryParameter::new ).collect( Collectors.toList() ) );
 	}
 
-	public static PendingQuery fromStructParameters( String sql, IStruct parameters ) {
+	/**
+	 * Creates a new PendingQuery instance from a SQL string with no parameters.
+	 *
+	 * @param sql         The SQL string to execute
+	 */
+	public PendingQuery( @Nonnull String sql ) {
+		this( sql, new ArrayList<>(), sql );
+	}
+
+	/**
+	 * Creates a new PendingQuery instance from a SQL string and an {@link IStruct} of named parameters.
+	 * The `IStruct` should map the `String` `name` to either an `Object` `value` or a `queryparam` `IStruct`.
+	 *
+	 * @param sql         The SQL string to execute
+	 * @param parameters  An `IStruct` of `String` `name` to either an `Object` `value` or a `queryparam` `IStruct`.
+	 */
+	public static @Nonnull PendingQuery fromStructParameters( @Nonnull String sql, @Nonnull IStruct parameters ) {
 		List<QueryParameter>	params	= new ArrayList<>();
 		Pattern					pattern	= Pattern.compile( ":\\w+" );
 		Matcher					matcher	= pattern.matcher( sql );
@@ -66,11 +125,26 @@ public class PendingQuery {
 		return new PendingQuery( matcher.replaceAll( "?" ), params, sql );
 	}
 
-	public List<Object> getParameterValues() {
+	/**
+	 * Returns a list of parameter `Object` values from the `List<QueryParameter>`.
+	 * @return A list of parameter values as `Object`s.
+	 */
+	public @Nonnull List<Object> getParameterValues() {
 		return this.parameters.stream().map( QueryParameter::getValue ).collect( Collectors.toList() );
 	}
 
-	public ExecutedQuery execute( Connection conn ) {
+	/**
+	 * Executes the PendingQuery on a given {@link Connection} and returns the results in an {@link ExecutedQuery} instance.
+	 *
+	 * @param conn The Connection to execute this PendingQuery on.
+	 *
+	 * @throws DatabaseException If a {@link SQLException} occurs, wraps it in a DatabaseException and throws.
+	 *
+	 * @return An ExecutedQuery instance with the results of this JDBC execution, as well as a link to this PendingQuery instance.
+	 *
+	 * @see ExecutedQuery
+	 */
+	public @Nonnull ExecutedQuery execute( @Nonnull Connection conn ) {
 		try ( PreparedStatement statement = conn.prepareStatement( this.sql, Statement.RETURN_GENERATED_KEYS ) ) {
 			// The param index starts from 1
 			for ( int i = 1; i <= this.parameters.size(); i++ ) {
@@ -94,7 +168,7 @@ public class PendingQuery {
 		} catch ( SQLException e ) {
 			throw new DatabaseException(
 			    e.getMessage(),
-			    "detail message",
+			    "detail message", // TODO: fill this out with the actual detail message
 			    String.valueOf( e.getErrorCode() ),
 			    e.getSQLState(),
 			    originalSql,
