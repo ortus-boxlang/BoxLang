3eadd26f2cdc6e2b2ea6bbdf20da73c7e9897095
jclausen <jclausen@ortussolutions.com>
2024-03-06 13:03:12 -0600

    implements BIF StringSort

---
+++ src/test/java/ortus/boxlang/runtime/bifs/global/string/StringSortTest.java
@@ -0,0 +1,85 @@
+
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ortus.boxlang.runtime.bifs.global.string;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import ortus.boxlang.runtime.BoxRuntime;
+import ortus.boxlang.runtime.context.IBoxContext;
+import ortus.boxlang.runtime.context.ScriptingRequestBoxContext;
+import ortus.boxlang.runtime.scopes.IScope;
+import ortus.boxlang.runtime.scopes.Key;
+import ortus.boxlang.runtime.scopes.VariablesScope;
+
+public class StringSortTest {
+
+	static BoxRuntime	instance;
+	IBoxContext			context;
+	IScope				variables;
+	static Key			result	= new Key( "result" );
+
+	@BeforeAll
+	public static void setUp() {
+		instance = BoxRuntime.getInstance( true );
+	}
+
+	@AfterAll
+	public static void teardown() {
+	}
+
+	@BeforeEach
+	public void setupEach() {
+		context		= new ScriptingRequestBoxContext( instance.getRuntimeContext() );
+		variables	= context.getScopeNearby( VariablesScope.name );
+	}
+
+	@DisplayName( "It should sort text ascending" )
+	@Test
+	public void testTextASC() {
+		instance.executeSource(
+		    """
+		        list = "bdca";
+		        result = stringSort( list );
+		    """,
+		    context );
+
+		assertEquals( "abcd", variables.getAsString( result ) );
+	}
+
+	@DisplayName( "It tests the member function for StringSort" )
+	@Test
+	public void testMemberFunction() {
+		instance.executeSource(
+		    """
+		        list = "bdca";
+		        result = list.stringSort();
+		    """,
+		    context );
+
+		assertEquals( "abcd", variables.getAsString( result ) );
+	}
+
+}
