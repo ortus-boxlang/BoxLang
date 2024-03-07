cbc6ff42bd79ea810f68e2f14d8b8214b8428a2e
Eric Peterson <eric@elpete.com>
2024-03-07 15:39:37 -0600

    Fix class loading issues in modules

--- src/main/resources/modules/test/bifs/Hola.java
+++ src/main/resources/modules/test/bifs/Hola.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package modules.test.bifs;
+package bifs;
 
 import ortus.boxlang.runtime.bifs.BIF;
 import ortus.boxlang.runtime.bifs.BoxBIF;
