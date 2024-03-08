192636b261159da841039e71d8dffd5983f3fc1b
Grant Copley <gcopley@ortussolutions.com>
2024-03-07 18:34:25 -0600

    Initial implementation of BIF queryColumnCount()

---
+++ src/test/java/ortus/boxlang/runtime/bifs/global/query/QueryColumnCountTest.java
@@ -0,0 +1,84 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ortus.boxlang.runtime.bifs.global.query;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import ortus.boxlang.runtime.BoxRuntime;
+import ortus.boxlang.runtime.context.IBoxContext;
+import ortus.boxlang.runtime.context.ScriptingRequestBoxContext;
+import ortus.boxlang.runtime.scopes.IScope;
+import ortus.boxlang.runtime.scopes.Key;
+import ortus.boxlang.runtime.scopes.VariablesScope;
+
+public class QueryColumnCountTest {
+
+	static BoxRuntime	instance;
+	IBoxContext			context;
+	IScope				variables;
+	static Key			result	= new Key( "result" );
+
+	@BeforeAll
+	public static void setUp() {
+		instance = BoxRuntime.getInstance( true );
+	}
+
+	@AfterAll
+	public static void teardown() {
+
+	}
+
+	@BeforeEach
+	public void setupEach() {
+		context		= new ScriptingRequestBoxContext( instance.getRuntimeContext() );
+		variables	= context.getScopeNearby( VariablesScope.name );
+	}
+
+	@DisplayName( "It should return the number of columns in a query" )
+	@Test
+	public void testQueryColumnCount() {
+		instance.executeSource(
+		    """
+		    query = QueryNew( "id,name", "integer,varchar" );
+		    result = QueryColumnCount( query );
+		    """,
+		    context );
+
+		assertThat( variables.get( result ) ).isEqualTo( 2 );
+	}
+
+	@DisplayName( "It should work using member function" )
+	@Test
+	public void testQueryColumnCountMemberFunction() {
+		instance.executeSource(
+		    """
+		    query = QueryNew( "id,name", "integer,varchar" );
+		    result = query.columnCount();
+		    """,
+		    context );
+
+		assertThat( variables.get( result ) ).isEqualTo( 2 );
+	}
+}
