23f0c5942a37a459db1d7352dee1533f58421cfa
Grant Copley <gcopley@ortussolutions.com>
2024-03-07 18:00:54 -0600

    Initial implementation of BIF querySome()

---
+++ src/main/java/ortus/boxlang/runtime/bifs/global/query/QuerySome.java
@@ -0,0 +1,69 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package ortus.boxlang.runtime.bifs.global.query;
+
+import ortus.boxlang.runtime.bifs.BIF;
+import ortus.boxlang.runtime.bifs.BoxBIF;
+import ortus.boxlang.runtime.bifs.BoxMember;
+import ortus.boxlang.runtime.context.IBoxContext;
+import ortus.boxlang.runtime.scopes.ArgumentsScope;
+import ortus.boxlang.runtime.scopes.Key;
+import ortus.boxlang.runtime.types.Argument;
+import ortus.boxlang.runtime.types.BoxLangType;
+import ortus.boxlang.runtime.types.util.ListUtil;
+
+@BoxBIF
+@BoxMember( type = BoxLangType.QUERY )
+public class QuerySome extends BIF {
+
+	/**
+	 * Constructor
+	 */
+	public QuerySome() {
+		super();
+		declaredArguments = new Argument[] {
+		    new Argument( true, "query", Key.query ),
+		    new Argument( true, "function", Key.callback ),
+		    new Argument( false, "boolean", Key.parallel, false ),
+		    new Argument( false, "integer", Key.maxThreads ),
+		    new Argument( Key.initialValue )
+		};
+	}
+
+	/**
+	 * This function calls a given closure/function with every element in a given query and returns true, if one of the closure calls returns true
+	 *
+	 * @param context   The context in which the BIF is being invoked.
+	 * @param arguments Argument scope for the BIF.
+	 *
+	 * @argument.query The query to iterate over
+	 *
+	 * @argument.callback The function to invoke for each item. The function will be passed 3 arguments: the row, the currentRow, the query.
+	 *
+	 * @argument.parallel Specifies whether the items can be executed in parallel
+	 *
+	 * @argument.maxThreads The maximum number of threads to use when parallel = true
+	 */
+	public Object _invoke( IBoxContext context, ArgumentsScope arguments ) {
+		return ListUtil.some(
+		    arguments.getAsQuery( Key.query ).toStructArray(),
+		    arguments.getAsFunction( Key.callback ),
+		    context,
+		    arguments.getAsBoolean( Key.parallel ),
+		    // we can't use the integer caster here because we need a cast null for the filter method signature
+		    ( Integer ) arguments.get( "maxThreads" )
+		);
+	}
+}
