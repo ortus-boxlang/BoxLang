2a5c26e687b8ab0eb02dd1d9f69d7dafa342552b
Luis Majano <lmajano@gmail.com>
2024-03-08 15:07:53 -0600

    another bif bites the dust

---
+++ src/main/java/ortus/boxlang/runtime/bifs/global/cache/CacheCount.java
@@ -0,0 +1,55 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package ortus.boxlang.runtime.bifs.global.cache;
+
+import java.util.Set;
+
+import ortus.boxlang.runtime.bifs.BIF;
+import ortus.boxlang.runtime.bifs.BoxBIF;
+import ortus.boxlang.runtime.cache.providers.ICacheProvider;
+import ortus.boxlang.runtime.cache.util.CacheExistsValidator;
+import ortus.boxlang.runtime.context.IBoxContext;
+import ortus.boxlang.runtime.scopes.ArgumentsScope;
+import ortus.boxlang.runtime.scopes.Key;
+import ortus.boxlang.runtime.types.Argument;
+
+@BoxBIF
+public class CacheCount extends BIF {
+
+	/**
+	 * Constructor
+	 */
+	public CacheCount() {
+		super();
+		declaredArguments = new Argument[] {
+		    new Argument( false, Argument.STRING, Key.cacheName, Key._DEFAULT, Set.of( new CacheExistsValidator() ) )
+		};
+	}
+
+	/**
+	 * Get how many items are in the cache. By default, the {@code cacheName} is set to {@code default}.
+	 *
+	 * @param context   The context in which the BIF is being invoked.
+	 * @param arguments Argument scope for the BIF.
+	 *
+	 * @argument.cacheName The cache name to retrieve the id from, defaults to {@code default}
+	 *
+	 * @return The number of items in the cache
+	 */
+	public Object _invoke( IBoxContext context, ArgumentsScope arguments ) {
+		ICacheProvider cache = cacheService.getCache( arguments.getAsKey( Key.cacheName ) );
+		return cache.getSize();
+	}
+}
