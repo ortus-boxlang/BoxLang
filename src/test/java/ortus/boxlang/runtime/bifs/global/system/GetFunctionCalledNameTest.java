f24195c8eac4c89fc001c2f30ea74d6b439af34b
Luis Majano <lmajano@gmail.com>
2024-03-18 18:12:25 +0100

    getFunctionCalledName completed

---
+++ src/test/java/ortus/boxlang/runtime/bifs/global/system/GetFunctionCalledNameTest.java
@@ -0,0 +1,100 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ortus.boxlang.runtime.bifs.global.system;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import ortus.boxlang.runtime.BoxRuntime;
+import ortus.boxlang.runtime.context.IBoxContext;
+import ortus.boxlang.runtime.context.ScriptingRequestBoxContext;
+import ortus.boxlang.runtime.scopes.IScope;
+import ortus.boxlang.runtime.scopes.Key;
+import ortus.boxlang.runtime.scopes.VariablesScope;
+
+public class GetFunctionCalledNameTest {
+
+	static BoxRuntime	instance;
+	IBoxContext			context;
+	IScope				variables;
+	static Key			result	= new Key( "result" );
+
+	@BeforeAll
+	public static void setUp() {
+		instance = BoxRuntime.getInstance( true );
+	}
+
+	@AfterAll
+	public static void teardown() {
+
+	}
+
+	@BeforeEach
+	public void setupEach() {
+		context		= new ScriptingRequestBoxContext( instance.getRuntimeContext() );
+		variables	= context.getScopeNearby( VariablesScope.name );
+	}
+
+	@DisplayName( "It can get the function called name when not in a function" )
+	@Test
+	public void testWithNoContext() {
+		instance.executeSource(
+		    """
+		    result = getFunctionCalledName()
+		    """,
+		    context );
+		assertThat( variables.get( result ) ).isEqualTo( "" );
+	}
+
+	@Test
+	@DisplayName( "Test with a function context" )
+	public void testWithFunctionContext() {
+		instance.executeSource(
+		    """
+		    function test() {
+		        return getFunctionCalledName();
+		    }
+		    result = test()
+		    """,
+		    context );
+		assertThat( variables.get( result ) ).isEqualTo( "test" );
+	}
+
+	@Test
+	@DisplayName( "Test with a function context and a new reference" )
+	public void testWithFunctionContextAndNewReference() {
+		instance.executeSource(
+		    """
+		       function test() {
+		           return getFunctionCalledName();
+		       }
+		    // Change reference
+		    anotherTest = test;
+		    result = anotherTest()
+		       """,
+		    context );
+		assertThat( variables.get( result ) ).isEqualTo( "anotherTest" );
+	}
+
+}
