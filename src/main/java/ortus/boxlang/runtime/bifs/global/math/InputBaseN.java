c53c2d21bb9aa08366d8abcc5406523bb57dcab5
Luis Majano <lmajano@gmail.com>
2024-03-18 22:17:13 +0100

    InputBaseN bif completed

---
+++ src/main/java/ortus/boxlang/runtime/bifs/global/math/InputBaseN.java
@@ -0,0 +1,69 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package ortus.boxlang.runtime.bifs.global.math;
+
+import java.util.Set;
+
+import ortus.boxlang.runtime.bifs.BIF;
+import ortus.boxlang.runtime.bifs.BoxBIF;
+import ortus.boxlang.runtime.bifs.BoxMember;
+import ortus.boxlang.runtime.context.IBoxContext;
+import ortus.boxlang.runtime.scopes.ArgumentsScope;
+import ortus.boxlang.runtime.scopes.Key;
+import ortus.boxlang.runtime.types.Argument;
+import ortus.boxlang.runtime.types.BoxLangType;
+import ortus.boxlang.runtime.validation.Validator;
+
+@BoxBIF
+@BoxMember( type = BoxLangType.STRING, name = "InputBaseN" )
+public class InputBaseN extends BIF {
+
+	/**
+	 * Constructor
+	 */
+	public InputBaseN() {
+		super();
+		declaredArguments = new Argument[] {
+		    new Argument( true, Argument.STRING, Key.string, Set.of( Validator.NON_EMPTY ) ),
+		    new Argument( true, Argument.NUMERIC, Key.radix, Set.of( Validator.min( 2 ), Validator.max( 36 ) ) )
+		};
+	}
+
+	/**
+	 * Converts a string, using the base specified by radix, to an integer.
+	 *
+	 * @param context   The context in which the BIF is being invoked.
+	 * @param arguments Argument scope for the BIF.
+	 *
+	 * @argument.string The string to convert to an integer.
+	 *
+	 * @argument.radix Base of the number represented by string, in the range 2-36.
+	 *
+	 * @return The integer value of the string.
+	 */
+	public Double _invoke( IBoxContext context, ArgumentsScope arguments ) {
+		String	string	= arguments.getAsString( Key.string );
+		int		radix	= arguments.getAsDouble( Key.radix ).intValue();
+
+		// Remove the 0x prefix if it exists.
+		if ( string.startsWith( "0x" ) ) {
+			string = string.substring( 2, string.length() );
+		}
+
+		// Convert the string to to radix but dont return it
+		return ( double ) Long.parseLong( string, radix );
+
+	}
+}
---
+++ src/test/java/ortus/boxlang/runtime/bifs/global/math/InputBaseNTest.java
@@ -0,0 +1,105 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ortus.boxlang.runtime.bifs.global.math;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import ortus.boxlang.runtime.BoxRuntime;
+import ortus.boxlang.runtime.context.IBoxContext;
+import ortus.boxlang.runtime.context.ScriptingRequestBoxContext;
+import ortus.boxlang.runtime.scopes.IScope;
+import ortus.boxlang.runtime.scopes.Key;
+import ortus.boxlang.runtime.scopes.VariablesScope;
+import ortus.boxlang.runtime.types.exceptions.BoxValidationException;
+
+public class InputBaseNTest {
+
+	static BoxRuntime	instance;
+	IBoxContext			context;
+	IScope				variables;
+	static Key			result	= new Key( "result" );
+
+	@BeforeAll
+	public static void setUp() {
+		instance = BoxRuntime.getInstance( true );
+	}
+
+	@AfterAll
+	public static void teardown() {
+
+	}
+
+	@BeforeEach
+	public void setupEach() {
+		context		= new ScriptingRequestBoxContext( instance.getRuntimeContext() );
+		variables	= context.getScopeNearby( VariablesScope.name );
+	}
+
+	@DisplayName( "It can convert strings to base n" )
+	@Test
+	public void testItCanConvertStringsToBaseN() {
+		instance.executeSource(
+		    """
+		    result = inputBaseN( "1010", 2 )
+		    """,
+		    context );
+		assertThat( variables.get( result ) ).isEqualTo( 10 );
+
+		instance.executeSource(
+		    """
+		    result = inputBaseN( "3FF", 16 )
+		      """,
+		    context );
+		assertThat( variables.get( result ) ).isEqualTo( 1023 );
+
+		instance.executeSource(
+		    """
+		    result = inputBaseN( "125", 10 )
+		      """,
+		    context );
+		assertThat( variables.get( result ) ).isEqualTo( 125 );
+	}
+
+	@DisplayName( "It can throw an error if the radix is < 2 and > 36" )
+	@Test
+	public void testItCanThrowAnErrorIfTheRadixIsLessThan2AndGreaterThan36() {
+		assertThrows( BoxValidationException.class, () -> {
+			instance.executeSource(
+			    """
+			    result = inputBaseN( "1010", 1 )
+			    """,
+			    context );
+		} );
+
+		assertThrows( BoxValidationException.class, () -> {
+			instance.executeSource(
+			    """
+			    result = inputBaseN( "1010", 37 )
+			    """,
+			    context );
+		} );
+	}
+
+}
