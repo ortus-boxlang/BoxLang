48bdec2c555bd0139575bd44733084fde16d38c4
Luis Majano <lmajano@gmail.com>
2024-04-11 23:17:02 -0500

    spotless apply

--- src/main/java/ortus/boxlang/runtime/components/ComponentDescriptor.java
+++ src/main/java/ortus/boxlang/runtime/components/ComponentDescriptor.java
@@ -133,7 +133,7 @@ public class ComponentDescriptor {
 					this.componentInstance = ( ( Component ) DynamicObject.of( this.componentClass )
 					    .invokeConstructor( ( IBoxContext ) null, new Object[] {} )
 					    .getTargetInstance() )
-					        .setName( name );
+					    .setName( name );
 					interceptorService.announce(
 					    BoxEvent.ON_COMPONENT_INSTANCE,
 					    new Struct(
