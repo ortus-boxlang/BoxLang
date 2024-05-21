ea928c0fa3365a093b9ff29f7bbb65c39e940dfa
Luis Majano <lmajano@gmail.com>
2024-05-21 16:43:40 +0200

    java docs

--- src/main/java/ortus/boxlang/runtime/bifs/global/conversion/ToNumeric.java
+++ src/main/java/ortus/boxlang/runtime/bifs/global/conversion/ToNumeric.java
@@ -1,3 +1,20 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ortus.boxlang.runtime.bifs.global.conversion;
 
 import ortus.boxlang.runtime.bifs.BIF;
@@ -13,6 +30,9 @@ import ortus.boxlang.runtime.types.Argument;
 @BoxBIF
 public class ToNumeric extends BIF {
 
+	/**
+	 * Constructor
+	 */
 	public ToNumeric() {
 		super();
 		declaredArguments = new Argument[] {
@@ -21,6 +41,18 @@ public class ToNumeric extends BIF {
 		};
 	}
 
+	/**
+	 * Cast a value to a number.
+	 *
+	 * @argument.value The value to cast.
+	 *
+	 * @argument.radix The radix to use when casting the value. Valid values are "bin", "oct", "dec", and "hex".
+	 *
+	 * @param context   The context in which the BIF is being executed.
+	 * @param arguments The arguments passed to the BIF.
+	 *
+	 * @return The numeric value of the input.
+	 */
 	public Object _invoke( IBoxContext context, ArgumentsScope arguments ) {
 		Object	inputValue	= arguments.get( Key.value );
 		Object	radixValue	= arguments.get( Key.radix );
@@ -36,6 +68,13 @@ public class ToNumeric extends BIF {
 		}
 	}
 
+	/**
+	 * Convert a radix string to an integer.
+	 *
+	 * @param radixString The radix string to convert.
+	 *
+	 * @return The integer value of the radix string.
+	 */
 	private int convertStringToRadix( String radixString ) {
 		switch ( radixString ) {
 			case "bin" :
