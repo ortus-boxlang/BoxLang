c4e8d7ba054b90d71a118266c00db1dffa82640d
Luis Majano <lmajano@gmail.com>
2024-05-28 22:41:58 +0200

    tons of string util bifs

--- src/main/java/ortus/boxlang/runtime/scopes/Key.java
+++ src/main/java/ortus/boxlang/runtime/scopes/Key.java
@@ -76,6 +76,7 @@ public class Key implements Comparable<Key>, Serializable {
 	public static final Key		addnewline						= Key.of( "addnewline" );
 	public static final Key		addToken						= Key.of( "addToken" );
 	public static final Key		algorithm						= Key.of( "algorithm" );
+	public static final Key		allow							= Key.of( "allow" );
 	public static final Key		allowRealPath					= Key.of( "allowRealPath" );
 	public static final Key		annotations						= Key.of( "annotations" );
 	public static final Key		ANONYMOUSCLOSURE				= Key.of( "ANONYMOUSCLOSURE" );
