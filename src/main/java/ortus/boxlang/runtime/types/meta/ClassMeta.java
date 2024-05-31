9b3165e5fad0e440cfee1de5e85c6c05a3668239
Luis Majano <lmajano@gmail.com>
2024-05-31 17:17:59 +0200

    size determinations and docs

--- src/main/java/ortus/boxlang/runtime/types/meta/ClassMeta.java
+++ src/main/java/ortus/boxlang/runtime/types/meta/ClassMeta.java
@@ -40,18 +40,31 @@ public class ClassMeta extends BoxMeta {
 
 	/**
 	 * Constructor
+	 *
+	 * @param target The target object this metadata is for
 	 */
 	public ClassMeta( IClassRunnable target ) {
 		super();
 		this.target	= target;
 		this.$class	= target.getClass();
 		// Assemble the metadata
-		var functions = new ArrayList<Object>();
-		// loop over target's variables scope and add metadata for each function
-		// TODO: If more than 20 do parallel
-		for ( var entry : target.getThisScope().entrySet() ) {
-			if ( entry.getValue() instanceof Function fun ) {
-				functions.add( ( ( FunctionMeta ) fun.getBoxMeta() ).meta );
+		var	functions	= new ArrayList<Object>();
+
+		// Functions are done depending on the size of the scope
+		var	thisScope	= target.getThisScope();
+		if ( thisScope.size() > 50 ) {
+			// use a stream
+			thisScope.entrySet()
+			    .stream()
+			    .filter( entry -> entry.getValue() instanceof Function )
+			    .forEach( entry -> {
+				    functions.add( ( ( FunctionMeta ) ( ( Function ) entry.getValue() ).getBoxMeta() ).meta );
+			    } );
+		} else {
+			for ( var entry : thisScope.entrySet() ) {
+				if ( entry.getValue() instanceof Function fun ) {
+					functions.add( ( ( FunctionMeta ) fun.getBoxMeta() ).meta );
+				}
 			}
 		}
 
@@ -94,7 +107,7 @@ public class ClassMeta extends BoxMeta {
 
 	/**
 	 * Get the variables scope directly
-	 * 
+	 *
 	 * @return The variables scope
 	 */
 	public IScope getVariablesScope() {
@@ -103,7 +116,7 @@ public class ClassMeta extends BoxMeta {
 
 	/**
 	 * Get the this scope directly
-	 * 
+	 *
 	 * @return The this scope
 	 */
 	public IScope getThisScope() {
