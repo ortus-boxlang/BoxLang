7c29923a367a402ac46d9030ce58202d43205250
michaelborn <michaelborn@users.noreply.github.com>
2024-07-26 16:10:13 +0000

    Apply cfformat changes

--- src/test/java/ortus/boxlang/runtime/util/FQNTest.java
+++ src/test/java/ortus/boxlang/runtime/util/FQNTest.java
@@ -1,4 +1,5 @@
 package ortus.boxlang.runtime.util;
+
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,6 +16,7 @@ public class FQNTest {
 		FQN fqn = new FQN( Paths.get( "/src/test/java/ortus/boxlang/runtime/util/FQNTest.java" ) );
 		assertEquals( "src.test.java.ortus.boxlang.runtime.util.FQNTest", fqn.toString() );
 	}
+
 	@DisplayName( "Can convert an absolute path to just a package path" )
 	@Test
 	void testItCanParseAbsolutePathToPackage() {
