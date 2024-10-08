ef3d015abdb64620a17234e6f5449e2e923ee572
Brad Wood <brad@bradwood.com>
2024-10-08 00:08:14 -0500

    BL-615

--- src/main/java/ortus/boxlang/runtime/interop/DynamicInteropService.java
+++ src/main/java/ortus/boxlang/runtime/interop/DynamicInteropService.java
@@ -1711,7 +1711,7 @@ public class DynamicInteropService {
 			// If we have the field, return its value, even if it's null
 			return getField( targetClass, targetInstance, name.getName() ).orElse( null );
 		} else if ( hasClassNoCase( targetClass, name.getName() ) ) {
-			return findClass( targetClass, name.getName() );
+			return DynamicObject.of( findClass( targetClass, name.getName() ) );
 		} else if ( targetClass.isEnum() ) {
 			return Enum.valueOf( ( Class<Enum> ) targetClass, name.getName() );
 		} else if ( getMethodNamesNoCase( targetClass, true ).contains( name.getName().toUpperCase() ) ) {
