8faff9626638111730b7b18b9f4b411b5a100480
jclausen <jclausen@ortussolutions.com>
2024-10-25 15:33:56 -0400

    BL-710 resolve - implements algorithms for random methods

--- src/main/java/ortus/boxlang/runtime/bifs/global/math/RandRange.java
+++ src/main/java/ortus/boxlang/runtime/bifs/global/math/RandRange.java
@@ -24,7 +24,6 @@ import ortus.boxlang.runtime.dynamic.casters.BigDecimalCaster;
 import ortus.boxlang.runtime.scopes.ArgumentsScope;
 import ortus.boxlang.runtime.scopes.Key;
 import ortus.boxlang.runtime.types.Argument;
-import ortus.boxlang.runtime.types.exceptions.BoxRuntimeException;
 import ortus.boxlang.runtime.types.util.MathUtil;
 
 @BoxBIF
@@ -49,13 +48,11 @@ public class RandRange extends BIF {
 	 * @param arguments Argument scope for the BIF.
 	 */
 	public Number _invoke( IBoxContext context, ArgumentsScope arguments ) {
-		if ( arguments.get( Key.algorithm ) != null ) {
-			throw new BoxRuntimeException( "The algorithm argument has not yet been implemented" );
-		}
 
 		Number	number1		= arguments.getAsNumber( Key.number1 );
 		Number	number2		= arguments.getAsNumber( Key.number2 );
 		Long	seed		= context.getAttachment( Key.bxRandomSeed );
+		String	algorithm	= arguments.getAsString( Key.algorithm );
 
 		boolean	isNumber1	= number1 instanceof BigDecimal;
 		boolean	isNumber2	= number2 instanceof BigDecimal;
@@ -63,14 +60,14 @@ public class RandRange extends BIF {
 		if ( isNumber1 || isNumber2 ) {
 			BigDecimal	bdl	= isNumber1 ? ( BigDecimal ) number1 : BigDecimalCaster.cast( number1 );
 			BigDecimal	bdr	= isNumber2 ? ( BigDecimal ) number2 : BigDecimalCaster.cast( number2 );
-			return bdl.add( new BigDecimal( Rand._invoke( seed ), MathUtil.getMathContext() ).multiply( bdr.subtract( bdl ) ) ).setScale( 0,
+			return bdl.add( new BigDecimal( Rand._invoke( algorithm, seed ), MathUtil.getMathContext() ).multiply( bdr.subtract( bdl ) ) ).setScale( 0,
 			    RoundingMode.DOWN );
 		}
 
 		// For integer values, ensure the range is inclusive of the top number
 		long	lower	= number1.longValue();
 		long	upper	= number2.longValue();
-		return lower + ( long ) ( Rand._invoke( seed ) * ( upper - lower + 1 ) );
+		return lower + ( long ) ( Rand._invoke( algorithm, seed ) ) * ( upper - lower + 1 );
 	}
 
 }
