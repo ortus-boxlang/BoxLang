82913653dd990b0edca8d6c2376e28657eeafac2
Brad Wood <brad@bradwood.com>
2024-11-04 12:20:14 -0600

    BL-733

--- src/test/java/ortus/boxlang/runtime/bifs/global/math/RandRangeTest.java
+++ src/test/java/ortus/boxlang/runtime/bifs/global/math/RandRangeTest.java
@@ -18,6 +18,8 @@
 
 package ortus.boxlang.runtime.bifs.global.math;
 
+import static com.google.common.truth.Truth.assertThat;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -107,6 +109,22 @@ public class RandRangeTest {
 		    	assert result <= 12;
 		    }
 		       """, context );
+	}
+
+	@DisplayName( "It includes upper and lower bound" )
+	@Test
+	public void testItIncludesUpperAndLowerBound() {
+		instance.executeSource(
+		    """
+		    result = []
+		       loop times=1000 {
+		          	result.append( randRange( 1, 3 ) );
+		    		 //result.append( rand() );
+		       }
+		          """, context );
+		assertThat( variables.getAsArray( result ) ).contains( 1L );
+		assertThat( variables.getAsArray( result ) ).contains( 2L );
+		assertThat( variables.getAsArray( result ) ).contains( 3L );
 	}
 
 }
