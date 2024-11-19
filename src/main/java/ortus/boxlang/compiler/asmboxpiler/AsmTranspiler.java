384e10c2b960e4d1a6f6dadfea5f5e0dfc4a4735
Rafael Winterhalter <rafael.wth@gmail.com>
2024-11-19 22:37:02 +0100

    Rebase to allow for debugging.

--- src/main/java/ortus/boxlang/compiler/asmboxpiler/AsmTranspiler.java
+++ src/main/java/ortus/boxlang/compiler/asmboxpiler/AsmTranspiler.java
@@ -13,8 +13,10 @@ import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.FieldInsnNode;
 import org.objectweb.asm.tree.InsnNode;
+import org.objectweb.asm.tree.InvokeDynamicInsnNode;
 import org.objectweb.asm.tree.LdcInsnNode;
 import org.objectweb.asm.tree.MethodInsnNode;
+import org.objectweb.asm.tree.MultiANewArrayInsnNode;
 import org.objectweb.asm.tree.TypeInsnNode;
 
 import ortus.boxlang.compiler.asmboxpiler.transformer.ReturnValueContext;
