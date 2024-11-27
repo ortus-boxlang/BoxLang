d3cc5f0823e556c7e55eacd540ee515047b1913c
Jon Clausen <jon_clausen@silowebworks.com>
2024-11-27 18:22:57 -0500

    BL-795 undo class method changes and implement QueryColumnListBIF and member

---
+++ src/main/java/ortus/boxlang/runtime/bifs/global/query/QueryColumnList.java
@@ -0,0 +1,52 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package ortus.boxlang.runtime.bifs.global.query;
+
+import ortus.boxlang.runtime.bifs.BIF;
+import ortus.boxlang.runtime.bifs.BoxBIF;
+import ortus.boxlang.runtime.bifs.BoxMember;
+import ortus.boxlang.runtime.context.IBoxContext;
+import ortus.boxlang.runtime.scopes.ArgumentsScope;
+import ortus.boxlang.runtime.scopes.Key;
+import ortus.boxlang.runtime.types.Argument;
+import ortus.boxlang.runtime.types.BoxLangType;
+import ortus.boxlang.runtime.types.util.ListUtil;
+
+@BoxBIF
+@BoxMember( type = BoxLangType.QUERY )
+public class QueryColumnList extends BIF {
+
+	/**
+	 * Constructor
+	 */
+	public QueryColumnList() {
+		super();
+		declaredArguments = new Argument[] {
+		    new Argument( true, "query", Key.query )
+		};
+	}
+
+	/**
+	 * This function returns the delimited column list of a query.
+	 *
+	 * @param context   The context in which the BIF is being invoked.
+	 * @param arguments Argument scope for the BIF.
+	 *
+	 * @argument.query The query to get the column names from
+	 */
+	public Object _invoke( IBoxContext context, ArgumentsScope arguments ) {
+		return arguments.getAsQuery( Key.query ).getColumnList();
+	}
+}
