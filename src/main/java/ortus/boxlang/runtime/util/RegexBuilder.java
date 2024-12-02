66c7aa6ec3dcbbfe263845dd425c07c95962ab32
Luis Majano <lmajano@gmail.com>
2024-12-02 21:48:19 +0100

    BL-784 WIP

---
+++ src/main/java/ortus/boxlang/runtime/util/RegexBuilder.java
@@ -0,0 +1,213 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ortus.boxlang.runtime.util;
+
+import java.util.Objects;
+import java.util.regex.Pattern;
+
+/**
+ * This class can be used as a utility to handle regular expressions.
+ * Add as many compiled patterns as needed and make sure you give them a meaningful name.
+ */
+public class RegexBuilder {
+
+	/**
+	 * Pattern Dictionary
+	 * Add as many patterns as needed, but make sure they are in all caps and alphabetically ordered.
+	 */
+	public static final Pattern	BACKSLASH			= Pattern.compile( "\\\\" );
+	public static final Pattern	COLON				= Pattern.compile( ":" );
+	public static final Pattern	PACKAGE_NAMES		= Pattern.compile( "[^a-zA-Z0-9$\\.]" );
+	public static final Pattern	PERIOD				= Pattern.compile( "\\." );
+	public static final Pattern	SLASH				= Pattern.compile( "/" );
+	public static final Pattern	STARTS_WITH_DIGIT	= Pattern.compile( "^\\d.*" );
+	public static final Pattern	TWO_DOTS			= Pattern.compile( "\\.{2}" );
+	public static final Pattern	WHITESPACE			= Pattern.compile( "\\s" );
+
+	/**
+	 * Build a matcher for the given pattern lookup
+	 *
+	 * @param input The input string to match against
+	 *
+	 * @return A new matcher instance
+	 */
+	public static RegexMatcher of( String input ) {
+		return new RegexMatcher( input );
+	}
+
+	/**
+	 * Build a matcher for the given input and string pattern
+	 *
+	 * @param input   The input string to match against
+	 * @param pattern The pattern to match against
+	 *
+	 * @return A new matcher instance
+	 */
+	public static RegexMatcher of( String input, String pattern ) {
+		return new RegexMatcher( input ).match( pattern );
+	}
+
+	/**
+	 * Build a matcher for the given input and string pattern
+	 *
+	 * @param input   The input string to match against
+	 * @param pattern The pattern to match against
+	 *
+	 * @return A new matcher instance
+	 */
+	public static RegexMatcher of( String input, Pattern pattern ) {
+		return new RegexMatcher( input ).match( pattern );
+	}
+
+	/**
+	 * The RegexMatcher class is used to match a pattern against an input string
+	 * by either using a pre-complied pattern or a string pattern that will be compiled on the fly.
+	 */
+	public static class RegexMatcher {
+
+		private Pattern			pattern;
+		private String			input;
+		private final String	original;
+
+		/**
+		 * Create a new matcher instance
+		 *
+		 * @param input The input string to match against
+		 */
+		private RegexMatcher( String input ) {
+			this.input		= input;
+			this.original	= input;
+		}
+
+		/**
+		 * Compile the pattern from the given pattern
+		 *
+		 * @param pattern The pattern to compile
+		 *
+		 * @return The matcher instance
+		 */
+		public RegexMatcher match( Pattern pattern ) {
+			this.pattern = pattern;
+			return this;
+		}
+
+		/**
+		 * Compile the pattern from the given string
+		 *
+		 * @param pattern The pattern to compile
+		 *
+		 * @return The matcher instance
+		 */
+		public RegexMatcher match( String pattern ) {
+			Objects.requireNonNull( pattern, "Pattern cannot be null" );
+			if ( pattern.isEmpty() ) {
+				throw new IllegalArgumentException( "Pattern cannot be empty" );
+			}
+			this.pattern = Pattern.compile( pattern );
+			return this;
+		}
+
+		/**
+		 * Check if the input string matches the pattern
+		 *
+		 * @return True if the input string matches the pattern, false otherwise
+		 */
+		public Boolean matches() {
+			return this.pattern.matcher( this.input ).matches();
+		}
+
+		/**
+		 * Replace all occurrences of the pattern in the input string with the replacement string
+		 *
+		 * @param replacement The replacement string
+		 *
+		 * @return The input string with all occurrences of the pattern replaced with the replacement string
+		 */
+		public RegexMatcher replaceAll( String replacement ) {
+			Objects.requireNonNull( replacement, "Replacement cannot be null" );
+			if ( replacement.isEmpty() ) {
+				throw new IllegalArgumentException( "Replacement cannot be empty" );
+			}
+			this.input = this.pattern
+			    .matcher( this.input )
+			    .replaceAll( replacement );
+
+			return this;
+		}
+
+		/**
+		 * Replace all occurrences of the pattern in the input string with the replacement string
+		 *
+		 * @param pattern     The pattern to match against
+		 * @param replacement The replacement string
+		 *
+		 * @return The input string with all occurrences of the pattern replaced with the replacement string
+		 */
+		public RegexMatcher replaceAll( Pattern pattern, String replacement ) {
+			Objects.requireNonNull( pattern, "Pattern cannot be null" );
+			Objects.requireNonNull( replacement, "Replacement cannot be null" );
+			this.input = pattern
+			    .matcher( this.input )
+			    .replaceAll( replacement );
+			return this;
+		}
+
+		/**
+		 * Replace all occurrences of the pattern in the input string with the replacement string
+		 *
+		 * @param replacement The replacement string
+		 *
+		 * @return The input string with all occurrences of the pattern replaced with the replacement string
+		 */
+		public String replaceAllAndGet( String replacement ) {
+			return this.replaceAll( replacement ).get();
+		}
+
+		/**
+		 * Replace all occurrences of the pattern in the input string with the replacement string
+		 *
+		 * @param replacement The replacement string
+		 *
+		 * @return The input string with all occurrences of the pattern replaced with the replacement string
+		 */
+		public String replaceAllAndGet( Pattern pattern, String replacement ) {
+			return this.replaceAll( pattern, replacement ).get();
+		}
+
+		/**
+		 * Get's the input string as modified by the matcher
+		 *
+		 * @return The input string
+		 */
+		public String get() {
+			return this.input;
+		}
+
+		/**
+		 * Reset the input string to the original value
+		 *
+		 * @return The matcher instance
+		 */
+		public RegexMatcher reset() {
+			this.input = this.original;
+			return this;
+		}
+
+	}
+
+}
