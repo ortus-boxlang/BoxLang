f78dedc73bd876a504665c734f2cb2e354ea6497
Luis Majano <lmajano@gmail.com>
2024-12-11 16:17:45 +0100

    no more logging

--- src/main/java/ortus/boxlang/runtime/loader/resolvers/BoxResolver.java
+++ src/main/java/ortus/boxlang/runtime/loader/resolvers/BoxResolver.java
@@ -318,7 +318,7 @@ public class BoxResolver extends BaseResolver {
 
 		// System.out.println( "mappings: " + mappings );
 		// System.out.println( "slashName: " + slashName );
-		getLogger().trace( "Resolving [{}] against mappings: [{}]", slashName, mappings );
+		// getLogger().trace( "Resolving [{}] against mappings: [{}]", slashName, mappings );
 
 		// Maybe if we have > 20 mappings we should use parallel streams
 
