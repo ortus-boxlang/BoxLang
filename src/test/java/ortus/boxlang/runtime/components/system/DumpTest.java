39828bce7d1a6e64551f46f30cdcaea7bb328e65
Scott Steinbeck <scottsteinbeck@gmail.com>
2025-05-28 11:02:10 -0700

    add in additional dump tests

--- src/test/java/ortus/boxlang/runtime/components/system/DumpTest.java
+++ src/test/java/ortus/boxlang/runtime/components/system/DumpTest.java
@@ -244,4 +244,307 @@ public class DumpTest {
 		// @formatter:on
 	}
 
+	@DisplayName( "It can dump an array list " )
+	@Test
+	public void testCanDumpAnArrayInHTML() {
+		// @formatter:off
+		instance.executeSource(
+		    """
+				val = [1,2,3,4,5];
+				dump( var = val, format = "html" );
+		    """,
+		    context );
+			assertThat( baos.toString().replaceAll( "[ \\t\\r\\n]", "" ) ).contains( "Array:5" );
+			// @formatter:on
+	}
+
+	@DisplayName( "It can dump an struct in html" )
+	@Test
+	public void testCanDumpAnStructHTML() {
+		// @formatter:off
+		instance.executeSource(
+		    """
+				val = {"a":1, "b":2, "c":3, "d":4, "e":5};
+				dump( var = val, format = "html" );
+		    """,
+		    context );
+			System.out.println( baos.toString() );
+ 		assertThat( baos.toString().replaceAll( "[ \\t\\r\\n]", "" ) ).contains( "12345" );
+		// @formatter:on
+	}
+
+	@DisplayName( "It can dump an Array data type" )
+	@Test
+	public void testCanDumpArray() {
+		// @formatter:off
+			instance.executeSource(
+				"""
+					val = [ "apple", "banana", "cherry" ];
+					dump( var = val, format = "html" );
+				""",
+				context );
+			// @formatter:on
+		assertThat( baos.toString() ).contains( "Array" );
+	}
+
+	@DisplayName( "It can dump a Boolean data type" )
+	@Test
+	public void testCanDumpBoolean() {
+		// @formatter:off
+			instance.executeSource(
+				"""
+					val = true;
+					dump( var = val, format = "html" );
+				""",
+				context );
+			// @formatter:on
+		assertThat( baos.toString() ).contains( "true" );
+	}
+
+	@DisplayName( "It can dump a BoxClass data type" )
+	@Test
+	public void testCanDumpBoxClass() {
+		// @formatter:off
+			instance.executeSource(
+				"""
+					cfc = new src.test.java.ortus.boxlang.runtime.components.system.TestDumpClass();
+					dump( var = cfc, format = "html" );
+				""",
+				context );
+			// @formatter:on
+		assertThat( baos.toString() ).contains( "UDF" );
+	}
+
+	@DisplayName( "It can dump a Class data type" )
+	@Test
+	public void testCanDumpClass() {
+		// @formatter:off
+			instance.executeSource(
+				"""
+					val = "Hello".getClass();
+					dump( var = val, format = "html" );
+				""",
+				context );
+			// @formatter:on
+		assertThat( baos.toString() ).contains( "Class" );
+	}
+
+	@DisplayName( "It can dump a DateTime data type" )
+	@Test
+	public void testCanDumpDateTime() {
+		// @formatter:off
+			instance.executeSource(
+				"""
+					// Assuming now() returns the current DateTime
+					val = now();
+					dump( var = val, format = "html" );
+				""",
+				context );
+			// @formatter:on
+		// Expect a date format marker (e.g. dashes)
+		assertThat( baos.toString() ).contains( "-" );
+	}
+
+	@DisplayName( "It can dump the Dump component" )
+	@Test
+	public void testCanDumpDump() {
+		// @formatter:off
+			instance.executeSource(
+				"""
+					dump( var = dump('testing innerdump'), format = "html" );
+				""",
+				context );
+			// @formatter:on
+		assertThat( baos.toString() ).contains( "innerdump" );
+	}
+
+	@DisplayName( "It can dump a Function data type" )
+	@Test
+	public void testCanDumpFunction() {
+		// @formatter:off
+			instance.executeSource(
+				"""
+					function add(a, b) { 
+						return a + b; 
+					}
+					dump( var = add(2+4), format = "html" );
+				""",
+				context );
+			// @formatter:on
+		assertThat( baos.toString() ).contains( "function" );
+	}
+
+	@DisplayName( "It can dump an Instant data type" )
+	@Test
+	public void testCanDumpInstant() {
+		// @formatter:off
+			instance.executeSource(
+				"""
+					import java.time.Instant;
+					val = Instant.now();
+					dump( var = val, format = "html" );
+				""",
+				context );
+			// @formatter:on
+		assertThat( baos.toString() ).contains( "Instant" );
+	}
+
+	@DisplayName( "It can dump a Key data type" )
+	@Test
+	public void testCanDumpKey() {
+		// @formatter:off
+			instance.executeSource(
+				"""
+					val = new ortus.boxlang.runtime.scopes.Key("testKey");
+					dump( var = val, format = "html" );
+				""",
+				context );
+			// @formatter:on
+		assertThat( baos.toString() ).contains( "testKey" );
+	}
+
+	@DisplayName( "It can dump a List data type" )
+	@Test
+	public void testCanDumpList() {
+		// @formatter:off
+			instance.executeSource(
+				"""
+					val = "apple, banana, cherry";
+					dump( var = val, format = "html" );
+				""",
+				context );
+			// @formatter:on
+		assertThat( baos.toString() ).contains( "apple" );
+	}
+
+	@DisplayName( "It can dump a Map data type" )
+	@Test
+	public void testCanDumpMap() {
+		// @formatter:off
+			instance.executeSource(
+				"""
+					import java.util.HashMap;
+					val = new HashMap();
+					val.put("key1", "value1");
+					val.put("key2", "value2");
+					val.put("key3", "value3");
+					dump( var = val, format = "html" );
+				""",
+				context );
+			// @formatter:on
+		assertThat( baos.toString() ).contains( "value1" );
+	}
+
+	@DisplayName( "It can dump a Null value" )
+	@Test
+	public void testCanDumpNull() {
+		// @formatter:off
+			instance.executeSource(
+				"""
+					val = null;
+					dump( var = val, format = "html" );
+				""",
+				context );
+			// @formatter:on
+		assertThat( baos.toString() ).contains( "null" );
+	}
+
+	@DisplayName( "It can dump a Number data type" )
+	@Test
+	public void testCanDumpNumber() {
+		// @formatter:off
+			instance.executeSource(
+				"""
+					val = 12345;
+					dump( var = val, format = "html" );
+				""",
+				context );
+			// @formatter:on
+		assertThat( baos.toString() ).contains( "12345" );
+	}
+
+	@DisplayName( "It can dump a Query data type" )
+	@Test
+	public void testCanDumpQuery() {
+		// @formatter:off
+			instance.executeSource(
+				"""
+					val = queryExecute("SELECT 1",{},{ "dbtype": "query" });
+					dump( var = val, format = "html" );
+				""",
+				context );
+			// @formatter:on
+		assertThat( baos.toString().toLowerCase() ).contains( "1" );
+	}
+
+	@DisplayName( "It can dump a String data type" )
+	@Test
+	public void testCanDumpString() {
+		// @formatter:off
+			instance.executeSource(
+				"""
+					val = "Hello, BoxLang";
+					dump( var = val, format = "html" );
+				""",
+				context );
+			// @formatter:on
+		assertThat( baos.toString() ).contains( "Hello, BoxLang" );
+	}
+
+	@DisplayName( "It can dump a StringBuffer data type" )
+	@Test
+	public void testCanDumpStringBuffer() {
+		// @formatter:off
+			instance.executeSource(
+				"""
+					val = new java.lang.StringBuffer("Buffer Content");
+					dump( var = val, format = "html" );
+				""",
+				context );
+			// @formatter:on
+		assertThat( baos.toString() ).contains( "Buffer Content" );
+	}
+
+	@DisplayName( "It can dump a Struct data type" )
+	@Test
+	public void testCanDumpStruct() {
+		// @formatter:off
+			instance.executeSource(
+				"""
+					val = { "first": "John", "last": "Doe" };
+					dump( var = val, format = "html" );
+				""",
+				context );
+			// @formatter:on
+		assertThat( baos.toString() ).contains( "John" );
+	}
+
+	@DisplayName( "It can dump a Throwable" )
+	@Test
+	public void testCanDumpThrowable() {
+		// @formatter:off
+			instance.executeSource(
+				"""
+					e =  new java.lang.Exception("Test Exception");
+					dump( var = e, format = "html" );
+				""",
+				context );
+			// @formatter:on
+		assertThat( baos.toString() ).contains( "Test Exception" );
+	}
+
+	@DisplayName( "It can dump a ToString result" )
+	@Test
+	public void testCanDumpToString() {
+		// @formatter:off
+			instance.executeSource(
+				"""
+					val = ("Numeric to string").toString();
+					dump( var = val, format = "html" );
+				""",
+				context );
+			// @formatter:on
+		assertThat( baos.toString() ).contains( "Numeric to string" );
+	}
+
 }
