c3473a9d3fc4d87d3b7ea764299568c5eee7953d
Luis Majano <lmajano@gmail.com>
2025-05-29 12:25:30 +0200

    BL-1357 #resolve Simple preFunctionInvoke interceptor throws errors due to recursing into itself

--- src/main/java/ortus/boxlang/runtime/interop/proxies/IInterceptorLambda.java
+++ src/main/java/ortus/boxlang/runtime/interop/proxies/IInterceptorLambda.java
@@ -20,22 +20,49 @@ package ortus.boxlang.runtime.interop.proxies;
 import ortus.boxlang.runtime.context.IBoxContext;
 import ortus.boxlang.runtime.dynamic.casters.BooleanCaster;
 import ortus.boxlang.runtime.events.Interceptor;
+import ortus.boxlang.runtime.scopes.Key;
 import ortus.boxlang.runtime.types.IStruct;
 import ortus.boxlang.runtime.types.exceptions.BoxRuntimeException;
 
+/**
+ * IInterceptorLambda is a proxy class that implements the IInterceptorLambda interface.
+ * It is used to intercept events in the BoxLang runtime.
+ * This is used to wrap closures or lambdas that are registered as interceptors.
+ */
 @Interceptor( autoLoad = false )
 public class IInterceptorLambda extends BaseProxy implements ortus.boxlang.runtime.events.IInterceptorLambda {
 
+	/**
+	 * Default constructor for IInterceptorLambda.
+	 * Initializes the proxy without any target or context.
+	 */
 	public IInterceptorLambda() {
 		super();
 	}
 
+	/**
+	 * Constructor for IInterceptorLambda.
+	 * Initializes the proxy with a target object, context, and method name.
+	 *
+	 * @param target  The target object that this proxy will wrap.
+	 * @param context The context in which this proxy operates.
+	 * @param method  The method name to be invoked on the target.
+	 */
 	public IInterceptorLambda( Object target, IBoxContext context, String method ) {
 		super( target, context, method );
 	}
 
 	@Override
 	public Boolean intercept( IStruct data ) {
+		// Does the data have a Function
+		if ( data.containsKey( Key.function ) && isFunctionTarget() ) {
+			// If so, then if the incoming function is the same as the target, then just return true
+			// If not, we will do recursive invocation
+			if ( data.get( Key.function ) == getAsFunction() ) {
+				return true;
+			}
+		}
+
 		try {
 			return BooleanCaster.cast( invoke( data ) );
 		} catch ( Exception e ) {
