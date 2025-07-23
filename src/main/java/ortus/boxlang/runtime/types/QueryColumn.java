868c38a1e8ae37c0113d5c67cfb90e7850f4af3f
Jon Clausen <jclausen@ortussolutions.com>
2025-07-23 18:38:40 -0400

    format

--- src/main/java/ortus/boxlang/runtime/types/QueryColumn.java
+++ src/main/java/ortus/boxlang/runtime/types/QueryColumn.java
@@ -99,7 +99,7 @@ public class QueryColumn implements IReferenceable, Serializable {
 	/**
 	 * The query this column is a part of
 	 */
-	private Query		query;
+	private Query				query;
 
 	/**
 	 * 0-based index of column in the
@@ -110,7 +110,7 @@ public class QueryColumn implements IReferenceable, Serializable {
 	/**
 	 * Metadata object
 	 */
-	public BoxMeta	$bx;
+	public BoxMeta				$bx;
 
 	/**
 	 * Serial version UID
