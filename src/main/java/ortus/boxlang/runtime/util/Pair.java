2cf0dd492d534e2b167cb9fa1800aedd9243e6ec
Luis Majano <lmajano@gmail.com>
2025-07-29 22:30:56 +0200

    documentation for class

--- src/main/java/ortus/boxlang/runtime/util/Pair.java
+++ src/main/java/ortus/boxlang/runtime/util/Pair.java
@@ -1,5 +1,3 @@
-package ortus.boxlang.runtime.util;
-
 /**
  * [BoxLang]
  *
@@ -17,24 +15,57 @@ package ortus.boxlang.runtime.util;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package ortus.boxlang.runtime.util;
+
+/**
+ * A simple immutable pair class for holding two related objects.
+ * <p>
+ * This is a utility class used throughout BoxLang for returning or storing two values together.
+ * The pair is typed, and both elements can be of any type.
+ * </p>
+ *
+ * @param <T> The type of the first element
+ * @param <U> The type of the second element
+ */
 public class Pair<T, U> {
 
 	private final T	first;
 	private final U	second;
 
+	/**
+	 * Constructs a new immutable Pair with the given values.
+	 *
+	 * @param first  The first value
+	 * @param second The second value
+	 */
 	public Pair( T first, U second ) {
 		this.first	= first;
 		this.second	= second;
 	}
 
+	/**
+	 * Returns the first value in the pair.
+	 *
+	 * @return The first value
+	 */
 	public T getFirst() {
 		return first;
 	}
 
+	/**
+	 * Returns the second value in the pair.
+	 *
+	 * @return The second value
+	 */
 	public U getSecond() {
 		return second;
 	}
 
+	/**
+	 * Returns a string representation of the pair in the form (first, second).
+	 *
+	 * @return String representation of the pair
+	 */
 	@Override
 	public String toString() {
 		return "(" + first + ", " + second + ")";
