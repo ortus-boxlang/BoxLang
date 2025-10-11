fb1f3cd5730d83b00e0e4fd134db50233d50f5f9
Jon Clausen <jclausen@ortussolutions.com>
2025-10-11 11:00:26 -0400

    add test for Compress BIF

--- src/main/java/ortus/boxlang/runtime/bifs/global/zip/Compress.java
+++ src/main/java/ortus/boxlang/runtime/bifs/global/zip/Compress.java
@@ -36,7 +36,7 @@ public class Compress extends BIF {
 	public Compress() {
 		super();
 		declaredArguments = new Argument[] {
-		    new Argument( true, Argument.STRING, Key.format, Set.of( Validator.valueOneOf( "zip", "gzip" ) ) ),
+		    new Argument( true, Argument.STRING, Key.format, "zip", Set.of( Validator.valueOneOf( "zip", "gzip" ) ) ),
 		    new Argument( true, Argument.STRING, Key.source ),
 		    new Argument( true, Argument.STRING, Key.destination ),
 		    new Argument( false, Argument.BOOLEAN, Key.includeBaseFolder, true ),
@@ -69,7 +69,7 @@ public class Compress extends BIF {
 	 * @param context   The context in which the BIF is being invoked.
 	 * @param arguments Argument scope for the BIF.
 	 *
-	 * @argument.format The format to use for the compression: zip or gzip.
+	 * @argument.format The format to use for the compression: zip or gzip. Default is zip.
 	 *
 	 * @argument.source The absolute path to the source file or folder to compress.
 	 *
