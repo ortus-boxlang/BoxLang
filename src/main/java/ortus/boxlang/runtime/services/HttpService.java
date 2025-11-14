e8a0a7a5b24c30d1198dc30881a158e02efa8c77
Luis Majano <lmajano@gmail.com>
2025-11-14 16:35:49 +0100

    fixed compile issues

--- src/main/java/ortus/boxlang/runtime/services/HttpService.java
+++ src/main/java/ortus/boxlang/runtime/services/HttpService.java
@@ -39,6 +39,7 @@ import ortus.boxlang.runtime.net.BoxHttpClient;
 import ortus.boxlang.runtime.scopes.Key;
 import ortus.boxlang.runtime.types.Array;
 import ortus.boxlang.runtime.types.IStruct;
+import ortus.boxlang.runtime.types.Struct;
 import ortus.boxlang.runtime.types.exceptions.BoxRuntimeException;
 import ortus.boxlang.runtime.util.EncryptionUtil;
 
@@ -236,7 +237,7 @@ public class HttpService extends BaseService {
 	public IStruct getAllClientStats() {
 		IStruct allStats = new Struct( false );
 		for ( Key key : this.clients.keySet() ) {
-			allStats.setValue( key.getName(), getClientStats( key ) );
+			allStats.put( key.getName(), getClientStats( key ) );
 		}
 		return allStats;
 	}
