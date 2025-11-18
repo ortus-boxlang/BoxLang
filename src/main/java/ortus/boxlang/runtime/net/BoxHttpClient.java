98dd45f64a4ea8f7668f08b32ab6cfd0c3cd4993
lmajano <137111+lmajano@users.noreply.github.com>
2025-11-18 16:12:48 +0000

    Apply cfformat changes

--- src/main/java/ortus/boxlang/runtime/net/BoxHttpClient.java
+++ src/main/java/ortus/boxlang/runtime/net/BoxHttpClient.java
@@ -1550,7 +1550,7 @@ public class BoxHttpClient {
 		 *     .method( "POST" )
 		 *     .header( "Content-Type", "application/json" )
 		 *     .onComplete( result -> {
-		 * 							} )
+		 * 	} )
 		 *     .send();
 		 *
 		 * IStruct result = request.getHttpResult();
