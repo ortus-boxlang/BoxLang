55acc6b8ab9e161226804516796ae8562e784333
Brad Wood <brad@bradwood.com>
2026-01-14 14:55:43 -0600

    BL-2101

--- src/main/java/ortus/boxlang/runtime/components/system/Application.java
+++ src/main/java/ortus/boxlang/runtime/components/system/Application.java
@@ -24,7 +24,7 @@ import ortus.boxlang.runtime.context.IBoxContext;
 import ortus.boxlang.runtime.scopes.Key;
 import ortus.boxlang.runtime.types.IStruct;
 
-@BoxComponent( description = "Define application-level settings and behavior" )
+@BoxComponent( allowsBody = false, description = "Define application-level settings and behavior" )
 public class Application extends Component {
 
 	/**
