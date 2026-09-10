fbd6f907baecaffe1da70a069a460a9099b0b48b
Eric Peterson <eric@elpete.com>
2026-09-10 10:05:06 -0600

    Fix numeric coercion of JDBC parameter scales

--- src/main/java/ortus/boxlang/runtime/jdbc/QueryParameter.java
+++ src/main/java/ortus/boxlang/runtime/jdbc/QueryParameter.java
@@ -17,6 +17,7 @@ package ortus.boxlang.runtime.jdbc;
 import ortus.boxlang.runtime.context.IBoxContext;
 import ortus.boxlang.runtime.dynamic.casters.BooleanCaster;
 import ortus.boxlang.runtime.dynamic.casters.CastAttempt;
+import ortus.boxlang.runtime.dynamic.casters.IntegerCaster;
 import ortus.boxlang.runtime.dynamic.casters.StringCaster;
 import ortus.boxlang.runtime.dynamic.casters.StructCaster;
 import ortus.boxlang.runtime.scopes.Key;
@@ -105,7 +106,7 @@ public class QueryParameter {
 		this.sqltype	= RegexBuilder.of( sqltype, RegexBuilder.CF_SQL ).replaceAllAndGet( "" ).toUpperCase().trim();
 		this.type		= QueryColumnType.fromString( this.sqltype );
 		this.maxLength	= param.getAsInteger( Key.maxLength );
-		this.scale		= param.getAsInteger( Key.scale );
+		this.scale		= param.get( Key.scale ) == null ? null : IntegerCaster.cast( param.get( Key.scale ) );
 	}
 
 	/**
